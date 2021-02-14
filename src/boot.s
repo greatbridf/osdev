7b880566be827bc826b3e2b8fdd00f502c95ec8a
greatbridf <guobingfang20040616@gmail.com>
2021-02-14 13:48:06 +0800

    protected mode

--- src/boot.s
+++ src/boot.s
@@ -1,6 +1,8 @@
 .text
 .code16
 
+# mbr
+
 .globl _start
 
 _start:
@@ -19,6 +21,8 @@ real_start:
 
     call read_data
 
+    ljmp $0x0050, $(loader_start-loader_start)
+
 die:
     hlt
     jmp die
@@ -35,10 +39,10 @@ string_hello:
 
 read_data_pack:
     .byte 0x10, 0
-    .word 2      # block count
+    .word 16     # block count (read 8k)
     .word 0x0000 # offset address
     .word 0x0050 # segment address
-    .long  0     # LBA to read
+    .long 1      # LBA to read
 
 stack_edge:
 .space 128
@@ -46,3 +50,78 @@ stack_base:
 
 .space 510 - (.-_start)
 .word 0xaa55
+
+
+# loader
+
+loader_start:
+# set segment registers
+    movw %cs, %ax
+    movw %ax, %ds
+
+# load gdt
+    cli
+    lgdt (asm_gdt_descriptor-loader_start)
+
+# enable protection enable (PE) bit
+    movl %cr0, %eax
+    orl $1, %eax
+    movl %eax, %cr0
+
+    ljmp $0x08, $0x0500 + (start_32bit-loader_start)
+
+.code32
+
+start_32bit:
+    movw $16, %ax
+    movw %ax, %ds
+    movw %ax, %ss
+
+# set up stack
+    movl $0x03ffffff, %ebp
+    movl $0x03ffffff, %esp
+
+# breakpoint
+#ifdef _DEBUG
+    xchgw %bx, %bx
+#endif
+
+# load gdt
+    cli
+    lgdt (gdt_descriptor-loader_start)
+    nop
+
+loader_halt:
+    hlt
+    jmp loader_halt
+
+asm_gdt_descriptor:
+    .word (3 * 8) - 1 # size
+    .long 0x0500+(asm_gdt_table-loader_start)  # address
+
+.globl asm_gdt_descriptor
+.type asm_gdt_descriptor @object
+.size asm_gdt_descriptor, (.-asm_gdt_descriptor)
+
+asm_gdt_table:
+    .8byte 0         # null descriptor
+
+    # code segment
+    .word 0x1000     # limit 0 :15
+    .word 0x0000     # base  0 :15
+    .byte 0x00       # base  16:23
+    .byte 0x9a       # access
+    .byte 0b11000000 # flag and limit 16:20
+    .byte 0x00       # base 24:31
+
+    # data segment
+    .word 0x1000     # limit 0 :15
+    .word 0x0000     # base  0 :15
+    .byte 0x00       # base  16:23
+    .byte 0x92       # access
+    .byte 0b11000000 # flag and limit 16:20
+    .byte 0x04       # base 24:31
+
+.space (512 * 31) - (.-loader_start)
+
+.space (512 * 2016) # fill 1m

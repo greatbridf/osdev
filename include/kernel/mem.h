bb030a6eb62e4a12d8531803b55d408800352ccd
greatbridf <guobingfang20040616@gmail.com>
2021-02-15 21:21:23 +0800

    detect memroy

--- include/kernel/mem.h
+++ include/kernel/mem.h
@@ -2,9 +2,10 @@
 
 #include <types/stdint.h>
 
+// don't forget to add the initial 1m to the total
 struct mem_size_info {
-    uint16_t n_16k_blks;
-    uint16_t n_64k_blks;
+    uint16_t n_1k_blks; // memory between 1m and 16m in 1k blocks
+    uint16_t n_64k_blks; // memory above 16m in 64k blocks
 };
 
 extern struct mem_size_info asm_mem_size_info;
--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,6 +1,7 @@
 #include <kernel_main.h>
 
 #include <asm/boot.h>
+#include <kernel/mem.h>
 #include <kernel/stdio.h>
 #include <kernel/vga.h>
 
@@ -15,6 +16,16 @@ void kernel_main(void)
     } else {
         vga_printk((const int8_t*)"A20 is NOT ON\n", 0x0fU);
     }
+
+    uint32_t mem_size = 0;
+    mem_size += 1024 * asm_mem_size_info.n_1k_blks;
+    mem_size += 64 * 1024 * asm_mem_size_info.n_64k_blks;
+
+    char buf[128] = { 0 };
+    snprintf(buf, 128, "Memory size: %d bytes (%d MB), 16k blocks: %d, 64k blocks: %d\n",
+        mem_size, mem_size / 1024 / 1024, (int32_t)asm_mem_size_info.n_1k_blks,
+        (int32_t)asm_mem_size_info.n_64k_blks);
+    vga_printk((const int8_t*)buf, 0x0fu);
 
     vga_printk((const int8_t*)"No work to do, halting...\n", 0x0fU);
 

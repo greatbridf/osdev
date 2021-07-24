b442fa0499a3beb6c02702300cd5714f8abdb519
greatbridf <guobingfang20040616@gmail.com>
2021-07-24 12:26:24 +0800

    chore: generate symbol file and debug info

--- make_symbol_table.sh
+++ make_symbol_table.sh
@@ -6,5 +6,5 @@ objdump -t ./build/kernel.out | dd of=./build/dump.txt
 
 awk '($1 ~ /[0-9]/) && ($4 != "*ABS*") && ($4 != ".text.bootsect") && ($3 != ".text.bootsect") && ($4 != ".magicnumber") {print $1 " " $NF}' ./build/dump.txt | dd of=./build/kernel.sym
 
-rm ./build/kernel.out
+#rm ./build/kernel.out
 rm ./build/dump.txt

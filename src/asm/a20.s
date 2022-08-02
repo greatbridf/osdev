e548724797b393c3325cc975a89abac3407bd474
greatbridf <greatbridf@icloud.com>
2022-08-03 01:14:10 +0800

    fix(a20): check a20 status without breaking code

--- src/asm/a20.s
+++ src/asm/a20.s
@@ -7,9 +7,20 @@ check_a20_on:
     pushal
     movl $0x112345, %edi
     movl $0x012345, %esi
+
+    movl (%esi), %eax
+    movl (%edi), %ecx
+
     movl %esi, (%esi)
     movl %edi, (%edi)
-    cmpsd
+    cmpsl
+
+    subl $4, %esi
+    subl $4, %edi
+
+    movl %eax, (%esi)
+    movl %ecx, (%edi)
+
     popal
     jne a20_on
     movl $0, %eax

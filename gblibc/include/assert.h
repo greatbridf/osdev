3e759fa4dd10c09edcd3bb5accb2c9ed9c7dc72e
greatbridf <greatbridf@icloud.com>
2023-03-16 04:12:27 +0000

    feat: assert

---
+++ gblibc/include/assert.h
@@ -0,0 +1,21 @@
+#ifndef __GBLIBC_ASSERT_H_
+#define __GBLIBC_ASSERT_H_
+
+#ifdef NDEBUG
+#define assert(st) ((void)(st))
+#else
+#define assert(st) ((void)((st) || (__assert_fail(#st, __FILE__, __LINE__, __func__), 0)))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+_Noreturn void
+__assert_fail(const char* statement, const char* file, int line, const char* func);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

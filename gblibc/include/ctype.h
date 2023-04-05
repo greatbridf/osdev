c4c0936a8fb57ad32b083fcb55e1e866542641b0
greatbridf <greatbridf@icloud.com>
2023-04-05 04:41:47 +0000

    feat(gblibc): add ctype

---
+++ gblibc/include/ctype.h
@@ -0,0 +1,18 @@
+#ifndef __GBLIBC_CTYPE_H_
+#define __GBLIBC_CTYPE_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int islower(int c);
+int isupper(int c);
+
+int tolower(int c);
+int toupper(int c);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif

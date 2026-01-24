c7c5cdac664168e2946b453708a1408729f72b2f
greatbridf <greatbridf@icloud.com>
2026-01-24 16:02:25 +0800

    test: rework test script and github workflow
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- script/test.sh
+++ script/test.sh
@@ -1,13 +1,71 @@
-#!/bin/sh
+#!/bin/bash
 
+LOGFILE="build/test-$$.log"
 SUCCESS_MSG="###$RANDOM :SuCCeSS: $RANDOM###"
 
-if [ "$ARCH" = "" ]; then
-    echo "Error: ARCH environment variable is not set." >&2
+die() {
+    echo "error: $1" >&2
     exit 1
-fi
+}
 
-printf "ls\necho \"$SUCCESS_MSG\"\npoweroff\n" \
-    | make test-run ARCH=$ARCH MODE=release QEMU=qemu-system-$ARCH \
-    | tee build/test-$$.log \
-    | grep "$SUCCESS_MSG" > /dev/null && echo TEST\ $$\ WITH\ ARCH=$ARCH\ PASSED
+runcmd() {
+    printf -v _cmd "%q" "$*"
+
+    cat <<EOF
+    expect {
+        "*/ # " { send "$_cmd\n" }
+        "*panicked" { exit 1 }
+    }
+EOF
+}
+
+wait_str() {
+    cat <<EOF
+    expect {
+        "*$*" {}
+        "*panicked" { exit 1 }
+    }
+EOF
+}
+
+wait_init_exit() {
+    echo 'expect "*init exited:" { exit 0 }'
+}
+
+cleanup() {
+    killall -KILL "qemu-system-$ARCH"
+}
+
+[ -z "$ARCH" ] && die "ARCH environment variable is not set"
+
+trap cleanup EXIT
+
+expect <<EOF | tee "$LOGFILE"
+
+set timeout 10
+spawn make test-run "ARCH=$ARCH" MODE=release "QEMU=qemu-system-$ARCH"
+
+$(runcmd ls -la)
+
+$(wait_str proc)
+
+$(runcmd echo \""$SUCCESS_MSG"\")
+
+$(wait_str "\n$SUCCESS_MSG")
+
+$(runcmd poweroff)
+
+$(wait_init_exit)
+
+EOF
+
+status=$?
+
+echo
+
+# shellcheck disable=SC2181
+if [ $status -ne 0 ]; then
+    echo "=== Test $$ with ARCH=$ARCH failed"
+else
+    echo "=== Test $$ with ARCH=$ARCH passed"
+fi

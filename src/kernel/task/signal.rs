greatbridf/osdev

commit e23c9eb1f24c572e19c9d275acf62e4ce0e2a1e0

runtime: new task sleep-wakeup method and some adaption
We use RUNNING to indicate that the task is on the cpu, and use READY to
indicate that the task could be further run again and therefore put into
the ready queue after one poll() call.

When the task is acquired from the ready queue and put onto cpu, it's
marked as RUNNING only, making it put suspended after we got the
Poll::Pending from the poll() call. If we (or others) call Waker::wake()
within the run, we'll set the READY flag then. And when we return from
the poll call, we could find it by a CAS and put it back to the ready
queue again.

We've also done some adaption work to the rest of the kernel, mainly to
remove *SOME* of the Task::block_on calls. But to completely remove it
is not possible for now. We should solve that in further few commits.

Signed-off-by: greatbridf <greatbridf@icloud.com>

src/kernel/task/signal.rs
@@ -9,7 +9,7 @@ use core::{cmp::Reverse, task::Waker};
 use eonix_hal::fpu::FpuState;
 use eonix_hal::traits::trap::RawTrapContext;
 use eonix_hal::trap::TrapContext;
-use eonix_runtime::task::Task;
+use eonix_runtime::scheduler::Runtime;
 use eonix_sync::AsProof as _;
 use intrusive_collections::UnsafeRef;
 use posix_types::signal::{SigSet, Signal};
@@ -226,15 +226,12 @@ impl SignalList {
 
                     // `SIGSTOP` can only be waken up by `SIGCONT` or `SIGKILL`.
                     // SAFETY: Preempt disabled above.
-                    {
+                    Runtime::block_till_woken(|waker| {
                         let mut inner = self.inner.lock();
-                        let waker = Waker::from(Task::current().clone());
-
-                        let old_waker = inner.stop_waker.replace(waker);
+                        let old_waker = inner.stop_waker.replace(waker.clone());
                         assert!(old_waker.is_none(), "We should not have a waker here");
-                    }
-
-                    Task::park_preempt_disabled();
+                    })
+                    .await;
 
                     if let Some(parent) = thread.process.parent.load() {
                         parent.notify(

crates/eonix_runtime/src/executor.rs
@@ -23,9 +23,7 @@ pub use stack::Stack;
 pub struct Executor(Option<Pin<Box<dyn TypeErasedExecutor>>>);
 
 trait TypeErasedExecutor: Send {
-    /// # Returns
-    /// Whether the executor has finished.
-    fn run(self: Pin<&mut Self>, cx: &mut Context<'_>) -> bool;
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()>;
 }
 
 struct RealExecutor<'a, F>
@@ -43,9 +41,9 @@ where
     F: Future + Send,
     F::Output: Send,
 {
-    fn run(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> bool {
+    fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
         if self.output_handle.as_ptr().is_null() {
-            return true;
+            return Poll::Ready(());
         }
 
         let future = unsafe {
@@ -53,21 +51,16 @@ where
             self.as_mut().map_unchecked_mut(|me| &mut me.future)
         };
 
-        match future.poll(cx) {
-            Poll::Ready(output) => {
-                if let Some(output_handle) = self.output_handle.upgrade() {
-                    output_handle.lock().commit_output(output);
+        future.poll(cx).map(|output| {
+            if let Some(output_handle) = self.output_handle.upgrade() {
+                output_handle.lock().commit_output(output);
 
-                    unsafe {
-                        // SAFETY: `output_handle` is Unpin.
-                        self.get_unchecked_mut().output_handle = Weak::new();
-                    }
+                unsafe {
+                    // SAFETY: `output_handle` is Unpin.
+                    self.get_unchecked_mut().output_handle = Weak::new();
                 }
-
-                true
             }
-            Poll::Pending => false,
-        }
+        })
     }
 }
 
@@ -79,7 +72,6 @@ impl Executor {
     {
         let output_handle = OutputHandle::new();
 
-        // TODO: accept futures with non 'static lifetimes.
         (
             Executor(Some(Box::pin(RealExecutor {
                 future,
@@ -90,16 +82,13 @@ impl Executor {
         )
     }
 
-    pub fn run(&mut self, cx: &mut Context<'_>) -> bool {
+    pub fn poll(&mut self, cx: &mut Context<'_>) -> Poll<()> {
         if let Some(executor) = self.0.as_mut() {
-            let finished = executor.as_mut().run(cx);
-            if finished {
+            executor.as_mut().poll(cx).map(|_| {
                 self.0.take();
-            }
-
-            finished
+            })
         } else {
-            true
+            Poll::Ready(())
         }
     }
 }

crates/eonix_runtime/src/scheduler.rs
@@ -1,14 +1,13 @@
 use crate::{
     executor::OutputHandle,
-    ready_queue::{cpu_rq, local_rq, ReadyQueue},
+    ready_queue::{local_rq, ReadyQueue},
     task::{Task, TaskAdapter, TaskHandle, TaskState},
 };
-use alloc::sync::Arc;
+use alloc::{sync::Arc, task::Wake};
 use core::{
     ops::{Deref, DerefMut},
     ptr::NonNull,
-    sync::atomic::Ordering,
-    task::Waker,
+    task::{Context, Poll, Waker},
 };
 use eonix_hal::processor::halt;
 use eonix_sync::{LazyLock, Spin, SpinIrq as _};
@@ -64,35 +63,6 @@ where
 }
 
 impl Runtime {
-    fn select_cpu_for_task(&self, task: &Task) -> usize {
-        task.cpu.load(Ordering::Relaxed) as _
-    }
-
-    pub fn activate(&self, task: &Arc<Task>) {
-        // Only one cpu can be activating the task at a time.
-        // TODO: Add some checks.
-
-        if task.on_rq.swap(true, Ordering::Acquire) {
-            // Lock the rq and check whether the task is on the rq again.
-            let cpuid = task.cpu.load(Ordering::Acquire);
-            let mut rq = cpu_rq(cpuid as _).lock_irq();
-
-            if !task.on_rq.load(Ordering::Acquire) {
-                // Task has just got off the rq. Put it back.
-                rq.put(task.clone());
-            } else {
-                // Task is already on the rq. Do nothing.
-                return;
-            }
-        } else {
-            // Task not on some rq. Select one and put it here.
-            let cpu = self.select_cpu_for_task(&task);
-            let mut rq = cpu_rq(cpu).lock_irq();
-            task.cpu.store(cpu as _, Ordering::Release);
-            rq.put(task.clone());
-        }
-    }
-
     pub fn spawn<F>(&self, future: F) -> JoinHandle<F::Output>
     where
         F: Future + Send + 'static,
@@ -104,36 +74,11 @@ impl Runtime {
         } = Task::new(future);
 
         self.add_task(task.clone());
-        self.activate(&task);
+        task.wake_by_ref();
 
         JoinHandle(output_handle)
     }
 
-    // /// Go to idle task. Call this with `preempt_count == 1`.
-    // /// The preempt count will be decremented by this function.
-    // ///
-    // /// # Safety
-    // /// We might never return from here.
-    // /// Drop all variables that take ownership of some resource before calling this function.
-    // pub fn schedule() {
-    //     assert_preempt_count_eq!(1, "Scheduler::schedule");
-
-    //     // Make sure all works are done before scheduling.
-    //     compiler_fence(Ordering::SeqCst);
-
-    //     // TODO!!!!!: Use of reference here needs further consideration.
-    //     //
-    //     // Since we might never return to here, we can't take ownership of `current()`.
-    //     // Is it safe to believe that `current()` will never change across calls?
-    //     unsafe {
-    //         // SAFETY: Preemption is disabled.
-    //         Scheduler::goto_scheduler(&Task::current().execution_context);
-    //     }
-    //     eonix_preempt::enable();
-    // }
-}
-
-impl Runtime {
     fn add_task(&self, task: Arc<Task>) {
         TASKS.lock_irq().insert(task);
     }
@@ -158,12 +103,18 @@ impl Runtime {
             return;
         };
 
-        match current.state.cmpxchg(TaskState::RUNNING, TaskState::READY) {
-            Ok(_) => {
+        match current.state.update(|state| match state {
+            TaskState::READY_RUNNING => Some(TaskState::READY),
+            TaskState::RUNNING => Some(TaskState::BLOCKED),
+            _ => {
+                unreachable!("Current task should be at least in RUNNING state, but got {state:?}")
+            }
+        }) {
+            Ok(TaskState::READY_RUNNING) => {
                 let current = unsafe {
                     Arc::from_raw(
                         CURRENT_TASK
-                            .get()
+                            .swap(None)
                             .expect("Current task should be present")
                             .as_ptr(),
                     )
@@ -171,13 +122,39 @@ impl Runtime {
 
                 rq.put(current);
             }
-            Err(old) => {
-                assert_eq!(
-                    old,
-                    TaskState::PARKED,
-                    "Current task should be in PARKED state"
-                );
+            Ok(_) => {}
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn block_till_woken(set_waker: impl FnOnce(&Waker)) -> impl Future<Output = ()> {
+        struct BlockTillWoken<F: FnOnce(&Waker)> {
+            set_waker: Option<F>,
+            slept: bool,
+        }
+
+        impl<F: FnOnce(&Waker)> Future for BlockTillWoken<F> {
+            type Output = ();
+
+            fn poll(self: core::pin::Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<()> {
+                if self.slept {
+                    Poll::Ready(())
+                } else {
+                    let (set_waker, slept) = unsafe {
+                        let me = self.get_unchecked_mut();
+                        (me.set_waker.take().unwrap(), &mut me.slept)
+                    };
+
+                    set_waker(cx.waker());
+                    *slept = true;
+                    Poll::Pending
+                }
             }
+        }
+
+        BlockTillWoken {
+            set_waker: Some(set_waker),
+            slept: false,
         }
     }
 
@@ -204,15 +181,23 @@ impl Runtime {
                 "Next task should be in READY state"
             );
 
-            CURRENT_TASK.set(NonNull::new(Arc::into_raw(next) as *mut _));
+            unsafe {
+                CURRENT_TASK.set(Some(NonNull::new_unchecked(Arc::into_raw(next) as *mut _)));
+            }
+
             drop(rq);
 
             // TODO: MAYBE we can move the release of finished tasks to some worker thread.
-            if Task::current().run() {
-                Task::current().state.set(TaskState::DEAD);
-                CURRENT_TASK.set(None);
+            if Task::current().poll().is_ready() {
+                let old_state = Task::current().state.swap(TaskState::DEAD);
+                assert!(
+                    old_state & TaskState::RUNNING != 0,
+                    "Current task should be at least in RUNNING state"
+                );
 
                 self.remove_task(&Task::current());
+
+                CURRENT_TASK.set(None);
             }
         }
     }

crates/eonix_runtime/src/task.rs
@@ -10,7 +10,7 @@ use atomic_unique_refcell::AtomicUniqueRefCell;
 use core::{
     ops::DerefMut,
     sync::atomic::{AtomicU32, Ordering},
-    task::{Context, Waker},
+    task::{Context, Poll, Waker},
 };
 use eonix_hal::processor::CPU;
 use eonix_sync::{Spin, SpinIrq};
@@ -59,7 +59,7 @@ impl Task {
         let task = Arc::new(Self {
             id: TaskId(ID.fetch_add(1, Ordering::Relaxed)),
             cpu: AtomicU32::new(CPU::local().cpuid() as u32),
-            state: TaskState::new(TaskState::RUNNING),
+            state: TaskState::new(TaskState::BLOCKED),
             executor: AtomicUniqueRefCell::new(executor),
             link_task_list: RBTreeAtomicLink::new(),
             link_ready_queue: LinkedListAtomicLink::new(),
@@ -71,30 +71,34 @@ impl Task {
         }
     }
 
-    /// # Returns
-    /// Whether the task has finished.
-    pub fn run(self: &Arc<Self>) -> bool {
+    pub fn poll(self: &Arc<Self>) -> Poll<()> {
         let mut executor_borrow = self.executor.borrow();
         let waker = Waker::from(self.clone());
         let mut cx = Context::from_waker(&waker);
 
-        executor_borrow.run(&mut cx)
+        executor_borrow.poll(&mut cx)
     }
 
     /// Get the stabilized lock for the task's run queue.
-    fn rq(&self) -> Option<impl DerefMut<Target = dyn ReadyQueue> + 'static> {
+    pub fn rq(&self) -> impl DerefMut<Target = dyn ReadyQueue> + 'static {
         loop {
             let cpu = self.cpu.load(Ordering::Relaxed);
             let rq = cpu_rq(cpu as usize).lock_irq();
 
-            if cpu == self.cpu.load(Ordering::Acquire) {
-                if self.link_ready_queue.is_linked() {
-                    return Some(rq);
-                } else {
-                    return None;
-                }
+            // We stabilize the task cpu with the cpu rq here for now.
+            if cpu != self.cpu.load(Ordering::Acquire) {
+                continue;
             }
+
+            return rq;
         }
+    }
+
+    pub fn block_on<F>(future: F) -> F::Output
+    where
+        F: Future,
+    {
+        todo!()
     }
 }
 
@@ -104,20 +108,18 @@ impl Wake for Task {
     }
 
     fn wake_by_ref(self: &Arc<Self>) {
-        if self
-            .state
-            .cmpxchg(TaskState::PARKED, TaskState::READY)
-            .is_err()
-        {
+        let Ok(old) = self.state.update(|state| match state {
+            TaskState::BLOCKED => Some(TaskState::READY),
+            TaskState::RUNNING => Some(TaskState::READY | TaskState::RUNNING),
+            TaskState::READY | TaskState::READY_RUNNING => None,
+            state => unreachable!("Waking a {state:?} task"),
+        }) else {
             return;
-        }
+        };
 
-        if let Some(mut rq) = self.rq() {
-            if self.state.get() != TaskState::PARKED {
-                return;
-            }
-
-            rq.put(self.clone());
+        if old == TaskState::BLOCKED {
+            // If the task was blocked, we need to put it back to the ready queue.
+            self.rq().put(self.clone());
         }
     }
 }

crates/eonix_runtime/src/task/task_state.rs
@@ -4,9 +4,10 @@ use core::sync::atomic::{AtomicU32, Ordering};
 pub struct TaskState(AtomicU32);
 
 impl TaskState {
-    pub const READY: u32 = 0;
-    pub const RUNNING: u32 = 1;
-    pub const PARKED: u32 = 2;
+    pub const BLOCKED: u32 = 0;
+    pub const READY: u32 = 1;
+    pub const RUNNING: u32 = 2;
+    pub const READY_RUNNING: u32 = TaskState::READY | TaskState::RUNNING;
     pub const DEAD: u32 = 1 << 31;
 
     pub(crate) const fn new(state: u32) -> Self {
@@ -17,16 +18,8 @@ impl TaskState {
         self.0.swap(state, Ordering::SeqCst)
     }
 
-    pub(crate) fn set(&self, state: u32) {
-        self.0.store(state, Ordering::SeqCst);
-    }
-
-    pub(crate) fn get(&self) -> u32 {
-        self.0.load(Ordering::SeqCst)
-    }
-
-    pub(crate) fn cmpxchg(&self, current: u32, new: u32) -> Result<u32, u32> {
+    pub(crate) fn update(&self, func: impl FnMut(u32) -> Option<u32>) -> Result<u32, u32> {
         self.0
-            .compare_exchange(current, new, Ordering::SeqCst, Ordering::SeqCst)
+            .fetch_update(Ordering::SeqCst, Ordering::SeqCst, func)
     }
 }

src/driver/serial.rs
@@ -3,14 +3,14 @@ mod io;
 use crate::{
     kernel::{
         block::make_device, console::set_console, constants::EIO, interrupt::register_irq_handler,
-        task::KernelStack, CharDevice, CharDeviceType, Terminal, TerminalDevice,
+        CharDevice, CharDeviceType, Terminal, TerminalDevice,
     },
     prelude::*,
 };
 use alloc::{collections::vec_deque::VecDeque, format, sync::Arc};
 use bitflags::bitflags;
 use core::pin::pin;
-use eonix_runtime::{run::FutureRun, scheduler::Scheduler};
+use eonix_runtime::scheduler::RUNTIME;
 use eonix_sync::{SpinIrq as _, WaitList};
 use io::SerialIO;
 
@@ -161,7 +161,7 @@ impl Serial {
             })?;
         }
 
-        Scheduler::get().spawn::<KernelStack, _>(FutureRun::new(Self::worker(port.clone())));
+        RUNTIME.spawn(Self::worker(port.clone()));
 
         let _ = set_console(terminal.clone());
         eonix_log::set_console(terminal.clone());

src/driver/virtio/riscv64.rs
@@ -1,23 +1,15 @@
 use super::virtio_blk::HAL;
-use crate::kernel::{
-    block::{make_device, BlockDevice},
-    mem::{AsMemoryBlock, MemoryBlock, Page},
-};
+use crate::kernel::block::{make_device, BlockDevice};
 use alloc::{sync::Arc, vec::Vec};
-use core::num::NonZero;
 use eonix_hal::arch_exported::fdt::FDT;
 use eonix_hal::mm::ArchPhysAccess;
 use eonix_log::{println_info, println_warn};
-use eonix_mm::{
-    address::{Addr, PAddr, PhysAccess},
-    paging::PFN,
-};
+use eonix_mm::address::{PAddr, PhysAccess};
 use eonix_runtime::task::Task;
 use eonix_sync::Spin;
 use virtio_drivers::{
     device::blk::VirtIOBlk,
     transport::{mmio::MmioTransport, Transport},
-    Hal,
 };
 
 pub fn init() {

src/kernel/interrupt.rs
@@ -1,5 +1,5 @@
 use super::mem::handle_kernel_page_fault;
-use super::timer::{should_reschedule, timer_interrupt};
+use super::timer::timer_interrupt;
 use crate::kernel::constants::EINVAL;
 use crate::prelude::*;
 use alloc::sync::Arc;
@@ -7,7 +7,6 @@ use eonix_hal::traits::fault::Fault;
 use eonix_hal::traits::trap::{RawTrapContext, TrapType};
 use eonix_hal::trap::TrapContext;
 use eonix_mm::address::{Addr as _, VAddr};
-use eonix_runtime::scheduler::Scheduler;
 use eonix_sync::SpinIrq as _;
 
 static IRQ_HANDLERS: Spin<[Vec<Arc<dyn Fn() + Send + Sync>>; 16]> =
@@ -51,15 +50,7 @@ pub fn interrupt_handler(trap_ctx: &mut TrapContext) {
         TrapType::Syscall { no, .. } => unreachable!("Syscall {} in kernel space.", no),
         TrapType::Fault(fault) => default_fault_handler(fault, trap_ctx),
         TrapType::Irq { callback } => callback(default_irq_handler),
-        TrapType::Timer { callback } => {
-            callback(timer_interrupt);
-
-            if eonix_preempt::count() == 0 && should_reschedule() {
-                // To make scheduler satisfied.
-                eonix_preempt::disable();
-                Scheduler::schedule();
-            }
-        }
+        TrapType::Timer { callback } => callback(timer_interrupt),
     }
 }
 

commit 45864d74327e43bd87803d9d2c4d1920fd643f71

signal: don't disable preemption before handling SIGSTOP
For some reason, we disabled preemptions before we set the waker and
yield. This is prohibited since we can't go to sleep with preemption
disabled.

After examining the previous commit, I think we used to call
Scheduler::schedule() to do context switches, which needs preemptions
disabled. When we move on to the new method, we just forgot to change
it. This should fix the issue.

Fixes: e23c9eb1f24c ("runtime: new task sleep-wakeup method and some adaption")
Signed-off-by: greatbridf <greatbridf@icloud.com>

src/kernel/task/signal.rs
@@ -128,7 +128,11 @@ impl SignalList {
         self.inner.lock().mask.unmask(mask)
     }
 
-    pub fn set_action(&self, signal: Signal, action: SignalAction) -> KResult<()> {
+    pub fn set_action(
+        &self,
+        signal: Signal,
+        action: SignalAction,
+    ) -> KResult<()> {
         if matches!(signal, SIGNAL_NOW!()) {
             return Err(EINVAL);
         }
@@ -141,7 +145,10 @@ impl SignalList {
         self.inner.lock().actions.get(signal)
     }
 
-    pub fn set_signal_waker(&self, waker: Option<UnsafeRef<dyn Fn() + Send + Sync>>) {
+    pub fn set_signal_waker(
+        &self,
+        waker: Option<UnsafeRef<dyn Fn() + Send + Sync>>,
+    ) {
         let mut inner = self.inner.lock();
         inner.signal_waker = waker;
     }
@@ -168,7 +175,11 @@ impl SignalList {
     }
 
     /// Handle signals in the context of `Thread::current()`.
-    pub async fn handle(&self, trap_ctx: &mut TrapContext, fpu_state: &mut FpuState) {
+    pub async fn handle(
+        &self,
+        trap_ctx: &mut TrapContext,
+        fpu_state: &mut FpuState,
+    ) {
         loop {
             let signal = {
                 let Some(signal) = self.inner.lock().pop() else { return };
@@ -182,7 +193,8 @@ impl SignalList {
                         old_mask
                     };
 
-                    let result = handler.handle(signal, old_mask, trap_ctx, fpu_state);
+                    let result =
+                        handler.handle(signal, old_mask, trap_ctx, fpu_state);
                     if result.is_err() {
                         self.inner.lock().mask = old_mask;
                     }
@@ -222,14 +234,15 @@ impl SignalList {
                         );
                     }
 
-                    eonix_preempt::disable();
-
                     // `SIGSTOP` can only be waken up by `SIGCONT` or `SIGKILL`.
                     // SAFETY: Preempt disabled above.
                     Runtime::block_till_woken(|waker| {
                         let mut inner = self.inner.lock();
                         let old_waker = inner.stop_waker.replace(waker.clone());
-                        assert!(old_waker.is_none(), "We should not have a waker here");
+                        assert!(
+                            old_waker.is_none(),
+                            "We should not have a waker here"
+                        );
                     })
                     .await;
 
@@ -293,15 +306,18 @@ impl SignalList {
         let old_fpu_state_vaddr = old_trap_ctx_vaddr + size_of::<TrapContext>();
         let old_mask_vaddr = old_fpu_state_vaddr + size_of::<FpuState>();
 
-        *trap_ctx = UserPointer::<TrapContext>::with_addr(old_trap_ctx_vaddr)?.read()?;
+        *trap_ctx = UserPointer::<TrapContext>::with_addr(old_trap_ctx_vaddr)?
+            .read()?;
 
         // Make sure that at least we won't crash the kernel.
         if !trap_ctx.is_user_mode() || !trap_ctx.is_interrupt_enabled() {
             return Err(EFAULT)?;
         }
 
-        *fpu_state = UserPointer::<FpuState>::with_addr(old_fpu_state_vaddr)?.read()?;
-        self.inner.lock().mask = UserPointer::<SigSet>::with_addr(old_mask_vaddr)?.read()?;
+        *fpu_state =
+            UserPointer::<FpuState>::with_addr(old_fpu_state_vaddr)?.read()?;
+        self.inner.lock().mask =
+            UserPointer::<SigSet>::with_addr(old_mask_vaddr)?.read()?;
 
         Ok(())
     }
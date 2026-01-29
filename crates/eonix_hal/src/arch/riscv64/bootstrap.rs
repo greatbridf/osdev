greatbridf/osdev

commit 4351cf55739f3a0d703814e67433903907b66a7b

partial work: fix riscv64 bootstrap

crates/eonix_hal/src/arch/riscv64/bootstrap.rs
@@ -1,11 +1,17 @@
+use super::{
+    config::{self, mm::*},
+    console::write_str,
+};
 use crate::{
     arch::{
-        cpu::CPU, fdt::init_dtb_and_fdt, mm::{ArchPhysAccess, PageAttribute64, PagingModeSv39, GLOBAL_PAGE_TABLE, V_KERNEL_BSS_START}
+        cpu::CPU,
+        fdt::{init_dtb_and_fdt, FdtExt},
+        mm::{ArchPhysAccess, FreeRam, PageAttribute64, GLOBAL_PAGE_TABLE},
     },
     bootstrap::BootStrapData,
     mm::{ArchMemory, ArchPagingMode, BasicPageAlloc, BasicPageAllocRef, ScopedAllocator},
 };
-use riscv::{asm::sfence_vma_all, register::{satp, sstatus::{self, FS}}};
+use core::arch::naked_asm;
 use core::{
     alloc::Allocator,
     arch::asm,
@@ -14,108 +20,117 @@ use core::{
 };
 use eonix_hal_traits::mm::Memory;
 use eonix_mm::{
-    address::{Addr as _, PAddr, PRange, VAddr, VRange},
+    address::{Addr as _, PAddr, PRange, PhysAccess, VAddr, VRange},
     page_table::{PageAttribute, PagingMode, PTE as _},
     paging::{Page, PageAccess, PageAlloc, PAGE_SIZE, PFN},
 };
 use eonix_percpu::PercpuArea;
-
-use super::{
-    config::{self, mm::*},
-    fdt::get_num_harts,
-    console::write_str,
+use fdt::Fdt;
+use riscv::{
+    asm::sfence_vma_all,
+    register::{
+        satp,
+        sstatus::{self, FS},
+    },
 };
 
-use core::arch::naked_asm;
+#[unsafe(link_section = ".bootstrap.stack")]
+static BOOT_STACK: [u8; 4096 * 16] = [0; 4096 * 16];
 
-#[unsafe(link_section = ".bootstack")]
-static mut BOOT_STACK: [u8; 4096 * 16] = [0; 4096 * 16];
+static BOOT_STACK_START: &'static [u8; 4096 * 16] = &BOOT_STACK;
 
 #[repr(C, align(4096))]
-struct BootPageTable([u64; PTES_PER_PAGE]);
+struct PageTable([u64; PTES_PER_PAGE]);
 
 /// map 0x8000 0000 to itself and 0xffff ffff 8000 0000
-#[unsafe(link_section = ".bootdata")]
-static mut BOOT_PAGE_TABLE: BootPageTable = {
+#[unsafe(link_section = ".bootstrap.page_table.1")]
+static BOOT_PAGE_TABLE: PageTable = {
     let mut arr: [u64; PTES_PER_PAGE] = [0; PTES_PER_PAGE];
-    arr[2] = (0x80000 << 10) | 0xcf;
-    arr[510] = (0x80000 << 10) | 0xcf;
-    BootPageTable(arr)
+    arr[0] = 0 | 0x2f;
+    arr[510] = 0 | 0x2f;
+    arr[511] = (0x80202 << 10) | 0x21;
+
+    PageTable(arr)
 };
 
-static AP_COUNT: AtomicUsize = AtomicUsize::new(0);
-static AP_STACK: AtomicUsize = AtomicUsize::new(0);
-static AP_SEM: AtomicBool = AtomicBool::new(false);
+#[unsafe(link_section = ".bootstrap.page_table.2")]
+#[used]
+static PT1: PageTable = {
+    let mut arr: [u64; PTES_PER_PAGE] = [0; PTES_PER_PAGE];
+    arr[510] = (0x80000 << 10) | 0x2f;
 
-unsafe extern "Rust" {
-    fn kernel_init();
-}
+    PageTable(arr)
+};
 
 /// bootstrap in rust
 #[unsafe(naked)]
 #[unsafe(no_mangle)]
-#[unsafe(link_section = ".text.entry")]
+#[unsafe(link_section = ".bootstrap.entry")]
 unsafe extern "C" fn _start(hart_id: usize, dtb_addr: usize) -> ! {
     naked_asm!(
         "
-            la    sp, {boot_stack}
-            la    t0, {page_table}
+            ld    sp, 2f
+            li    t0, 0x10000
+            add   sp, sp, t0
+            ld    t0, 3f
             srli  t0, t0, 12
-            li    t1, 8 << 60
+            li    t1, 9 << 60
             or    t0, t0, t1
             csrw  satp, t0
             sfence.vma
-            li    t2, {virt_ram_offset}
-            or    sp, sp, t2
-            la    t3, riscv64_start
-            or    t3, t3, t2
-            jalr  t3                      // call riscv64_start
+            ld    t0, 4f
+            jalr  t0                      // call riscv64_start
+
+            .pushsection .bootstrap.data, \"aw\", @progbits
+            2:
+            .8byte {boot_stack}
+            3:
+            .8byte {page_table}
+            4:
+            .8byte {riscv64_start}
+            .popsection
         ",
         boot_stack = sym BOOT_STACK,
         page_table = sym BOOT_PAGE_TABLE,
-        virt_ram_offset = const KIMAGE_OFFSET,
+        riscv64_start = sym riscv64_start,
     )
 }
 
-/// TODO: 
-/// linker，现在VMA和LMA不对
-/// 设置中断
+/// TODO:
 /// 启动所有的cpu
-#[unsafe(no_mangle)]
-pub unsafe extern "C" fn riscv64_start(hart_id: usize, dtb_addr: usize) -> ! {
-    write_str("hello\n");
+pub unsafe extern "C" fn riscv64_start(hart_id: usize, dtb_addr: PAddr) -> ! {
+    enable_fpu();
+    let fdt = Fdt::from_ptr(ArchPhysAccess::as_ptr(dtb_addr).as_ptr())
+        .expect("Failed to parse DTB from static memory.");
 
     let real_allocator = RefCell::new(BasicPageAlloc::new());
     let alloc = BasicPageAllocRef::new(&real_allocator);
 
-    for range in ArchMemory::free_ram() {
+    for range in fdt.present_ram().free_ram() {
         real_allocator.borrow_mut().add_range(range);
     }
 
     setup_kernel_page_table(&alloc);
     unsafe {
-        init_dtb_and_fdt(dtb_addr)
-    };
-    enable_sse();
-    
-    let num_harts = get_num_harts();
-    config::smp::set_num_harts(num_harts);
+        init_dtb_and_fdt(dtb_addr);
+    }
 
     setup_cpu(&alloc, hart_id);
 
     // TODO: set up interrupt, smp
-    //ScopedAllocator::new(&mut [0; 1024])
-    //    .with_alloc(|mem_alloc| bootstrap_smp(mem_alloc, &real_allocator));
+    ScopedAllocator::new(&mut [0; 1024])
+        .with_alloc(|mem_alloc| bootstrap_smp(mem_alloc, &real_allocator));
 
     unsafe extern "Rust" {
         fn _eonix_hal_main(_: BootStrapData) -> !;
     }
 
-    let start = &raw mut BOOT_STACK as usize + KIMAGE_OFFSET;
+    let start = unsafe {
+        ((&BOOT_STACK_START) as *const &'static [u8; 4096 * 16]).read_volatile() as *const _
+            as usize
+    };
     let bootstrap_data = BootStrapData {
-        early_stack: PRange::new(
-            PAddr::from(start),
-            PAddr::from(start + 4096 * 16)),
+        early_stack: PRange::new(PAddr::from(start), PAddr::from(start + 4096 * 16)),
         allocator: Some(real_allocator),
     };
 
@@ -131,7 +146,7 @@ unsafe extern "C" {
 
 /// TODO:
 /// 对kernel image添加更细的控制，或者不加也行
-pub fn setup_kernel_page_table(alloc: impl PageAlloc) {
+fn setup_kernel_page_table(alloc: impl PageAlloc) {
     let global_page_table = &GLOBAL_PAGE_TABLE;
 
     let attr = PageAttribute::WRITE
@@ -140,40 +155,33 @@ pub fn setup_kernel_page_table(alloc: impl PageAlloc) {
         | PageAttribute::GLOBAL
         | PageAttribute::PRESENT;
 
-    // Map Physical memory 128Gb, add a 0xFFFF_FFC0_0000_0000 offset
-    // use 1 Gb size page
-    for (idx, pte) in global_page_table
-        .iter_kernel_levels(VRange::from(VAddr::from(PHYS_MAP_VIRT)).grow(MEMORY_SIZE), &PagingModeSv39::LEVELS[..=0])
-        .enumerate()
-    {
-        pte.set(PFN::from(idx * 0x40000), PageAttribute64::from(attr));
-    }
-
-    // Map 2 MB kernel image
-    for (idx, pte) in global_page_table
-        .iter_kernel(VRange::from(VAddr::from(KIMAGE_VIRT_BASE)).grow(KIMAGE_PAGES as usize * 0x1000))
-        .enumerate()
-    {
-        pte.set(PFN::from(idx + 0x80200), PageAttribute64::from(attr));
-    }
+    const KERNEL_BSS_START: VAddr = VAddr::from(0xffffffff40000000);
 
     // Map kernel BSS
     for pte in global_page_table.iter_kernel_in(
-        VRange::from(V_KERNEL_BSS_START).grow(BSS_LENGTH as usize),
+        VRange::from(KERNEL_BSS_START).grow(BSS_LENGTH as usize),
         ArchPagingMode::LEVELS,
         &alloc,
     ) {
         let page = Page::alloc_in(&alloc);
+
+        let attr = {
+            let mut attr = attr.clone();
+            attr.remove(PageAttribute::EXECUTE);
+            attr
+        };
         pte.set(page.into_raw(), attr.into());
     }
 
+    sfence_vma_all();
+
     unsafe {
-        core::ptr::write_bytes(V_KERNEL_BSS_START.addr() as *mut (), 0, BSS_LENGTH as usize);
+        core::ptr::write_bytes(KERNEL_BSS_START.addr() as *mut (), 0, BSS_LENGTH as usize);
     }
 
     unsafe {
         satp::set(
-            satp::Mode::Sv39,
+            satp::Mode::Sv48,
             0,
             usize::from(PFN::from(global_page_table.addr())),
         );
@@ -181,7 +189,7 @@ pub fn setup_kernel_page_table(alloc: impl PageAlloc) {
     sfence_vma_all();
 }
 
-pub fn enable_sse() {
+fn enable_fpu() {
     unsafe {
         // FS (Floating-point Status) Initial (0b01)
         sstatus::set_fs(FS::Initial);
@@ -216,11 +224,13 @@ fn setup_cpu(alloc: impl PageAlloc, hart_id: usize) {
     unsafe {
         cpu.as_mut().init(hart_id);
     }
-    
+
     percpu_area.register(cpu.cpuid());
 }
 
 /// TODO
-fn bootstrap_smp(alloc: impl Allocator, page_alloc: &RefCell<BasicPageAlloc>) {
+fn bootstrap_smp(alloc: impl Allocator, page_alloc: &RefCell<BasicPageAlloc>) {}
 
+pub fn early_console_write(s: &str) {
+    write_str(s);
 }

crates/eonix_hal/src/arch/riscv64/fdt.rs
@@ -1,40 +1,62 @@
-extern crate alloc;
-
-use alloc::string::String;
-
-use fdt::Fdt;
-use spin::Once;
-
+use super::mm::{ArchPhysAccess, PresentRam};
 use crate::arch::riscv64::config::mm::KIMAGE_OFFSET;
+use core::sync::atomic::{AtomicPtr, Ordering};
+use eonix_mm::address::{PAddr, PRange, PhysAccess};
+use eonix_sync_base::LazyLock;
+use fdt::Fdt;
 
-static GLOBAL_FDT: Once<Fdt<'static>> = Once::new();
+static DTB_VIRT_PTR: AtomicPtr<u8> = AtomicPtr::new(core::ptr::null_mut());
+pub static FDT: LazyLock<Fdt<'static>> = LazyLock::new(|| unsafe {
+    Fdt::from_ptr(DTB_VIRT_PTR.load(Ordering::Acquire))
+        .expect("Failed to parse DTB from static memory.")
+});
 
-pub unsafe fn init_dtb_and_fdt(dtb_pa: usize) {
-    let dtb_virt_ptr = (dtb_pa + KIMAGE_OFFSET) as *const u8;
+pub trait FdtExt {
+    fn harts(&self) -> impl Iterator<Item = usize>;
 
-    GLOBAL_FDT.call_once(|| {
-        unsafe { Fdt::from_ptr(dtb_virt_ptr)
-            .expect("Failed to parse DTB from static memory.") }
-    });
-}
-
-pub fn get_global_fdt() -> &'static Fdt<'static> {
-    GLOBAL_FDT.get().expect("Global FDT has not been initialized yet")
-}
-
-#[derive(Debug, Clone)]
-pub struct HartInfo {
-    pub hart_id: usize,
-    pub compatible: Option<String>,
-}
-
-pub fn get_num_harts() -> usize {
-    let fdt = get_global_fdt();
-    let dtb_cpus = fdt.cpus();
-    let mut num_harts = 0;
-
-    for _cpu in dtb_cpus {
-        num_harts += 1;
+    fn hart_count(&self) -> usize {
+        self.harts().count()
     }
-    num_harts
+
+    fn present_ram(&self) -> impl Iterator<Item = PRange>;
+}
+
+impl FdtExt for Fdt<'_> {
+    fn harts(&self) -> impl Iterator<Item = usize> {
+        self.cpus().map(|cpu| cpu.ids().all()).flatten()
+    }
+
+    fn present_ram(&self) -> impl Iterator<Item = PRange> + PresentRam {
+        struct Present<I>(I);
+        impl<I> PresentRam for Present<I> where I: Iterator<Item = PRange> {}
+        impl<I> Iterator for Present<I>
+        where
+            I: Iterator<Item = PRange>,
+        {
+            type Item = PRange;
+
+            fn next(&mut self) -> Option<Self::Item> {
+                self.0.next()
+            }
+        }
+
+        let mut index = 0;
+        Present(core::iter::from_fn(move || {
+            self.memory()
+                .regions()
+                .filter_map(|region| {
+                    region.size.map(|len| {
+                        PRange::from(PAddr::from(region.starting_address as usize)).grow(len)
+                    })
+                })
+                .skip(index)
+                .next()
+                .inspect(|_| index += 1)
+        }))
+    }
+}
+
+pub unsafe fn init_dtb_and_fdt(dtb_paddr: PAddr) {
+    let dtb_virt_ptr = ArchPhysAccess::as_ptr(dtb_paddr);
+    DTB_VIRT_PTR.store(dtb_virt_ptr.as_ptr(), Ordering::Release);
 }

crates/eonix_hal/src/arch/riscv64/mm.rs
@@ -1,24 +1,20 @@
-extern crate alloc;
-
-
-use crate::{arch::{fdt::get_global_fdt, riscv64::config::mm::KIMAGE_OFFSET}, traits::mm::Memory};
+use super::{
+    config::mm::{PHYS_MAP_VIRT, ROOT_PAGE_TABLE_PFN},
+    fdt::{FdtExt, FDT},
+};
+use crate::{arch::riscv64::config::mm::KIMAGE_OFFSET, traits::mm::Memory};
 use core::{marker::PhantomData, ptr::NonNull};
 use eonix_mm::{
-    address::{Addr as _, AddrOps, PAddr, PRange, PhysAccess, VAddr}, page_table::{
-        PageAttribute, PageTable, PageTableLevel, PagingMode, RawAttribute, RawPageTable, TableAttribute, PTE
-    }, paging::{NoAlloc, Page, PageBlock, PFN}
+    address::{Addr as _, AddrOps, PAddr, PRange, PhysAccess, VAddr},
+    page_table::{
+        PageAttribute, PageTable, PageTableLevel, PagingMode, RawAttribute, RawPageTable,
+        TableAttribute, PTE,
+    },
+    paging::{NoAlloc, Page, PageBlock, PFN},
 };
 use eonix_sync_base::LazyLock;
 use fdt::Fdt;
 use riscv::{asm::sfence_vma_all, register::satp};
-
-use super::config::mm::{
-    ROOT_PAGE_TABLE_PFN,
-    PHYS_MAP_VIRT
-};
-
-pub const P_KIMAGE_START: PAddr = PAddr::from_val(0x80200000);
-pub const V_KERNEL_BSS_START: VAddr = VAddr::from(0xffffffff00000000);
 
 pub const PAGE_TABLE_BASE: PFN = PFN::from_val(ROOT_PAGE_TABLE_PFN);
 pub static GLOBAL_PAGE_TABLE: LazyLock<PageTable<ArchPagingMode, NoAlloc, ArchPhysAccess>> =
@@ -54,9 +50,9 @@ pub struct PTE64(pub u64);
 #[derive(Clone, Copy)]
 pub struct PageAttribute64(u64);
 
-pub struct RawPageTableSv39<'a>(NonNull<PTE64>, PhantomData<&'a ()>);
+pub struct RawPageTableSv48<'a>(NonNull<PTE64>, PhantomData<&'a ()>);
 
-pub struct PagingModeSv39;
+pub struct PagingModeSv48;
 
 pub struct ArchPhysAccess;
 
@@ -76,19 +72,20 @@ impl PTE for PTE64 {
     }
 }
 
-impl PagingMode for PagingModeSv39 {
+impl PagingMode for PagingModeSv48 {
     type Entry = PTE64;
-    type RawTable<'a> = RawPageTableSv39<'a>;
+    type RawTable<'a> = RawPageTableSv48<'a>;
     const LEVELS: &'static [PageTableLevel] = &[
+        PageTableLevel::new(39, 9),
         PageTableLevel::new(30, 9),
         PageTableLevel::new(21, 9),
         PageTableLevel::new(12, 9),
     ];
 }
 
-pub type ArchPagingMode = PagingModeSv39;
+pub type ArchPagingMode = PagingModeSv48;
 
-impl<'a> RawPageTable<'a> for RawPageTableSv39<'a> {
+impl<'a> RawPageTable<'a> for RawPageTableSv48<'a> {
     type Entry = PTE64;
 
     fn index(&self, index: u16) -> &'a Self::Entry {
@@ -203,7 +200,7 @@ impl From<PageAttribute> for PageAttribute64 {
                 PageAttribute::GLOBAL => raw_attr |= PA_G,
                 PageAttribute::COPY_ON_WRITE => raw_attr |= PA_COW,
                 PageAttribute::MAPPED => raw_attr |= PA_MMAP,
-                PageAttribute::ANONYMOUS => {},
+                PageAttribute::ANONYMOUS => {}
                 _ => unreachable!("Invalid page attribute"),
             }
         }
@@ -260,41 +257,20 @@ impl PhysAccess for ArchPhysAccess {
 }
 
 impl Memory for ArchMemory {
-    /// TODO: fuck, I don't know how to fix
-    fn present_ram() -> impl Iterator<Item = eonix_mm::address::PRange> {
-        let fdt: &'static Fdt<'static> = get_global_fdt();
-
-        let memory_node = fdt.memory().clone();
-
-        let owned_ranges: alloc::vec::Vec<PRange> = memory_node.regions() 
-            .map(|mem_region| {
-                let base = mem_region.starting_address as usize;
-                let size = mem_region.size.expect("this region has no memory") as usize;
-                PRange::from(PAddr::from(base as usize)).grow(size as usize)
-            }).collect();
-        
-        owned_ranges.into_iter()
+    fn present_ram() -> impl Iterator<Item = PRange> {
+        FDT.present_ram()
     }
 
-    fn free_ram() -> impl Iterator<Item = eonix_mm::address::PRange> {
+    fn free_ram() -> impl Iterator<Item = PRange> {
         unsafe extern "C" {
             fn __kernel_start();
             fn __kernel_end();
         }
 
         let kernel_end = PAddr::from(__kernel_end as usize - KIMAGE_OFFSET);
-        let paddr_after_kimage_aligned = kernel_end.ceil_to(0x2000000);
+        let paddr_after_kimage_aligned = kernel_end.ceil_to(0x200000);
 
-        core::iter::once(PRange::new(
-            kernel_end,
-            paddr_after_kimage_aligned,
-        ))
-
-        /*core::iter::once(PRange::new(
-            kernel_end,
-            paddr_after_kimage_aligned,
-        ))
-        .chain(
+        core::iter::once(PRange::new(kernel_end, paddr_after_kimage_aligned)).chain(
             Self::present_ram()
                 .filter(move |range| range.end() > paddr_after_kimage_aligned)
                 .map(move |range| {
@@ -305,15 +281,41 @@ impl Memory for ArchMemory {
                         range
                     }
                 }),
-        )*/
+        )
     }
 }
 
-pub type DefaultPagingMode = PagingModeSv39;
+pub type DefaultPagingMode = PagingModeSv48;
 
-pub fn setup_kernel_satp() {
-    unsafe {
-        satp::set(satp::Mode::Sv39, 0, PFN::from(ROOT_PAGE_TABLE_PFN).into());
+pub trait PresentRam: Iterator<Item = PRange> {}
+
+pub trait FreeRam: PresentRam {
+    fn free_ram(self) -> impl Iterator<Item = PRange>;
+}
+
+impl<T> FreeRam for T
+where
+    T: PresentRam,
+{
+    fn free_ram(self) -> impl Iterator<Item = PRange> {
+        unsafe extern "C" {
+            fn __kernel_start();
+            fn __kernel_end();
+        }
+
+        let kernel_end = PAddr::from(__kernel_end as usize - KIMAGE_OFFSET);
+        let paddr_after_kimage_aligned = kernel_end.ceil_to(0x200000);
+
+        core::iter::once(PRange::new(kernel_end, paddr_after_kimage_aligned)).chain(
+            self.filter(move |range| range.end() > paddr_after_kimage_aligned)
+                .map(move |range| {
+                    if range.start() < paddr_after_kimage_aligned {
+                        let (_, right) = range.split_at(paddr_after_kimage_aligned);
+                        right
+                    } else {
+                        range
+                    }
+                }),
+        )
     }
-    sfence_vma_all();
 }

arch/src/riscv64/fdt.rs
@@ -1,99 +1,0 @@
-extern crate alloc;
-
-use alloc::{string::String, vec::Vec};
-
-#[derive(Debug, Clone)]
-pub struct HartInfo {
-    pub hart_id: usize,
-    pub compatible: Option<String>,
-}
-
-#[derive(Debug)]
-pub struct CpuManager {
-    pub num_harts: usize,
-    pub harts: Vec<HartInfo>,
-    pub boot_hart_id: usize,
-}
-
-impl CpuManager {
-    pub fn from_fdt(dtb_addr: usize, boot_hart_id: usize) -> Self {
-        let fdt = unsafe {
-            fdt::Fdt::from_ptr(dtb_addr as *const u8)
-                .expect("Failed to parse device tree from dtb_addr")
-        };
-
-        let mut harts_info = Vec::new();
-        let mut num_harts = 0;
-
-        if let Some(cpus_node) = fdt.find_node("/cpus") {
-            for cpu_node in cpus_node.children() {
-                let mut is_riscv_cpu = false;
-                let compatible_string: Option<String> = 
-                    if let Some(compatible_prop) = cpu_node
-                        .properties()
-                        .find(|p| p.name == "compatible")
-                    {
-                        if let Some(s) = compatible_prop.as_str() {
-                            if s.starts_with("riscv,") {
-                                is_riscv_cpu = true;
-                            }
-                            Some(String::from(s))
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    };
-
-                if is_riscv_cpu {
-                    let hart_id = if let Some(hart_id_prop) = cpu_node
-                        .properties()
-                        .find(|p| p.name == "hartid")
-                    {
-                        hart_id_prop.as_usize()
-                            .expect("hartid property in CPU node is not a valid integer")
-                    } else if let Some(reg_prop) = cpu_node
-                        .properties()
-                        .find(|p| p.name == "reg")
-                    {
-                        reg_prop.as_usize()
-                            .expect("reg property in CPU node is not a valid integer for hartid")
-                    } else {
-                        panic!("CPU node {:?} does not have a 'hartid' or 'reg' property required for its ID", cpu_node.name);
-                    };
-
-                    let hart_info = HartInfo {
-                        hart_id,
-                        compatible: compatible_string,
-                    };
-                    harts_info.push(hart_info);
-                    num_harts += 1;
-                }
-            }
-        } else {
-            panic!("Device Tree does not contain a /cpus node!");
-        }
-
-        harts_info.sort_by_key(|h| h.hart_id);
-
-        Self {
-            num_harts,
-            harts: harts_info,
-            boot_hart_id,
-        }
-    }
-}
-
-pub fn get_num_harts(dtb_addr: usize) -> usize {
-    let fdt = unsafe {
-        fdt::Fdt::from_ptr(dtb_addr as *const u8)
-            .expect("Failed to parse device tree from dtb_addr")
-    };
-    let dtb_cpus = fdt.cpus();
-    let mut num_harts = 0;
-
-    for _cpu in dtb_cpus {
-        num_harts += 1;
-    }
-    num_harts
-}

commit 5814c006a88ed2397b3e085e86e59c179f6cdcb2

riscv64: rewrite FDT and present free memory parsing
Strip out the memory used by the kernel and FDT data out of free memory
block returned by FDT.

Closes: #54 ("Random kernel freezing on process creation / exiting")
Fixes: 4351cf55739f3 ("partial work: fix riscv64 bootstrap")
Signed-off-by: greatbridf <greatbridf@icloud.com>

crates/eonix_hal/src/arch/riscv64/bootstrap.rs
@@ -29,7 +29,7 @@ use super::time::set_next_timer;
 use crate::arch::cpu::CPU;
 use crate::arch::fdt::{init_dtb_and_fdt, FdtExt, FDT};
 use crate::arch::mm::{
-    ArchPagingMode, ArchPhysAccess, FreeRam, PageAccessImpl, PageAttribute64,
+    ArchPagingMode, ArchPhysAccess, PageAccessImpl, PageAttribute64,
     RawPageTableSv48, GLOBAL_PAGE_TABLE,
 };
 use crate::bootstrap::BootStrapData;
@@ -113,13 +113,12 @@ unsafe extern "C" fn _start(hart_id: usize, dtb_addr: usize) {
 }
 
 pub unsafe extern "C" fn riscv64_start(hart_id: usize, dtb_addr: PAddr) -> ! {
-    let fdt = Fdt::from_ptr(ArchPhysAccess::as_ptr(dtb_addr).as_ptr())
-        .expect("Failed to parse DTB from static memory.");
+    let fdt = unsafe { FdtExt::new(dtb_addr) };
 
     let real_allocator = RefCell::new(BasicPageAlloc::new());
     let alloc = BasicPageAllocRef::new(&real_allocator);
 
-    for range in fdt.present_ram().free_ram() {
+    for range in fdt.free_ram() {
         real_allocator.borrow_mut().add_range(range);
     }
 

crates/eonix_hal/src/arch/riscv64/fdt.rs
@@ -1,62 +1,95 @@
-use super::mm::{ArchPhysAccess, PresentRam};
-use crate::arch::riscv64::config::mm::KIMAGE_OFFSET;
-use core::sync::atomic::{AtomicPtr, Ordering};
-use eonix_mm::address::{PAddr, PRange, PhysAccess};
+use core::ops::Deref;
+use core::sync::atomic::{AtomicPtr, AtomicUsize, Ordering};
+
+use eonix_mm::address::{Addr, AddrOps, PAddr, PRange, PhysAccess};
 use eonix_sync_base::LazyLock;
 use fdt::Fdt;
 
-static DTB_VIRT_PTR: AtomicPtr<u8> = AtomicPtr::new(core::ptr::null_mut());
-pub static FDT: LazyLock<Fdt<'static>> = LazyLock::new(|| unsafe {
-    Fdt::from_ptr(DTB_VIRT_PTR.load(Ordering::Acquire))
-        .expect("Failed to parse DTB from static memory.")
+use super::mm::ArchPhysAccess;
+use crate::arch::riscv64::config::mm::KIMAGE_OFFSET;
+use crate::extern_symbol_addr;
+
+static DTB_PADDR: AtomicUsize = AtomicUsize::new(0);
+pub static FDT: LazyLock<FdtExt> = LazyLock::new(|| unsafe {
+    FdtExt::new(PAddr::from_val(DTB_PADDR.load(Ordering::Relaxed)))
 });
 
-pub trait FdtExt {
-    fn harts(&self) -> impl Iterator<Item = usize>;
-
-    fn hart_count(&self) -> usize {
-        self.harts().count()
-    }
-
-    fn present_ram(&self) -> impl Iterator<Item = PRange>;
+pub struct FdtExt {
+    fdt: Fdt<'static>,
+    range: PRange,
 }
 
-impl FdtExt for Fdt<'_> {
-    fn harts(&self) -> impl Iterator<Item = usize> {
+impl FdtExt {
+    /// # Safety
+    /// The caller MUST ensure that [`addr`] points to valid FDT.
+    pub unsafe fn new(addr: PAddr) -> Self {
+        let fdt = unsafe {
+            Fdt::from_ptr(ArchPhysAccess::as_ptr(addr).as_ptr())
+                .expect("Failed to parse DTB from static memory.")
+        };
+
+        Self {
+            range: PRange::from(addr).grow(fdt.total_size()),
+            fdt,
+        }
+    }
+
+    pub fn harts(&self) -> impl Iterator<Item = usize> {
         self.cpus().map(|cpu| cpu.ids().all()).flatten()
     }
 
-    fn present_ram(&self) -> impl Iterator<Item = PRange> + PresentRam {
-        struct Present<I>(I);
-        impl<I> PresentRam for Present<I> where I: Iterator<Item = PRange> {}
-        impl<I> Iterator for Present<I>
-        where
-            I: Iterator<Item = PRange>,
-        {
-            type Item = PRange;
+    pub fn hart_count(&self) -> usize {
+        self.harts().count()
+    }
 
-            fn next(&mut self) -> Option<Self::Item> {
-                self.0.next()
-            }
-        }
-
+    pub fn present_ram(&self) -> impl Iterator<Item = PRange> {
         let mut index = 0;
-        Present(core::iter::from_fn(move || {
-            self.memory()
+
+        core::iter::from_fn(move || {
+            let item = self
+                .memory()
                 .regions()
                 .filter_map(|region| {
-                    region.size.map(|len| {
-                        PRange::from(PAddr::from(region.starting_address as usize)).grow(len)
-                    })
+                    let start = PAddr::from(region.starting_address as usize);
+                    Some(start).zip(region.size)
                 })
-                .skip(index)
-                .next()
-                .inspect(|_| index += 1)
-        }))
+                .map(|(start, len)| PRange::from(start).grow(len))
+                .nth(index);
+
+            index += 1;
+            item
+        })
+    }
+
+    pub fn free_ram(&self) -> impl Iterator<Item = PRange> {
+        let kernel_end = extern_symbol_addr!(__kernel_end) - KIMAGE_OFFSET;
+        let kernel_end = PAddr::from(kernel_end).ceil();
+
+        // TODO: move this to some platform-specific crate
+        self.present_ram().map(move |mut range| {
+            // Strip out parts before __kernel_end
+            if range.overlap_with(&PRange::from(kernel_end)) {
+                (_, range) = range.split_at(kernel_end);
+            }
+
+            // Strip out part after the FDT
+            if range.overlap_with(&self.range) {
+                (range, _) = range.split_at(self.range.start());
+            }
+
+            range
+        })
+    }
+}
+
+impl Deref for FdtExt {
+    type Target = Fdt<'static>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.fdt
     }
 }
 
 pub unsafe fn init_dtb_and_fdt(dtb_paddr: PAddr) {
-    let dtb_virt_ptr = ArchPhysAccess::as_ptr(dtb_paddr);
-    DTB_VIRT_PTR.store(dtb_virt_ptr.as_ptr(), Ordering::Release);
+    DTB_PADDR.store(dtb_paddr.addr(), Ordering::Relaxed);
 }

crates/eonix_hal/src/arch/riscv64/mm.rs
@@ -275,45 +275,11 @@ impl Memory for ArchMemory {
     }
 
     fn free_ram() -> impl Iterator<Item = PRange> {
-        FDT.present_ram().free_ram()
+        FDT.free_ram()
     }
 }
 
 pub type DefaultPagingMode = PagingModeSv48;
-
-pub trait PresentRam: Iterator<Item = PRange> {}
-
-pub trait FreeRam: PresentRam {
-    fn free_ram(self) -> impl Iterator<Item = PRange>;
-}
-
-impl<T> FreeRam for T
-where
-    T: PresentRam,
-{
-    fn free_ram(self) -> impl Iterator<Item = PRange> {
-        let kernel_end = extern_symbol_addr!(__kernel_end) - KIMAGE_OFFSET;
-        let kernel_end = PAddr::from(kernel_end).ceil();
-
-        let paddr_after_kimage_aligned = kernel_end.ceil_to(0x200000);
-
-        core::iter::once(PRange::new(kernel_end, paddr_after_kimage_aligned))
-            .chain(
-                self.filter(move |range| {
-                    range.end() > paddr_after_kimage_aligned
-                })
-                .map(move |range| {
-                    if range.start() < paddr_after_kimage_aligned {
-                        let (_, right) =
-                            range.split_at(paddr_after_kimage_aligned);
-                        right
-                    } else {
-                        range
-                    }
-                }),
-            )
-    }
-}
 
 #[inline(always)]
 pub fn flush_tlb(vaddr: usize) {
greatbridf/osdev

commit ebd3d1224c017fa41eabb862769ce50359849923

change(x86): optimize bootstrap code, remove kinit.cpp
some changes are made to the memory layout to make the
initialization more fluent.

fixes the `TaskContext` that `call[0-5]` might cause the function
stack frames to be unaligned.

src/kernel_init.rs
@@ -1,0 +1,226 @@
+use crate::{
+    kernel::{
+        self,
+        cpu::init_localcpu,
+        mem::{AsMemoryBlock, GlobalPageAlloc, KernelPageAccess, RawPage},
+    },
+    kernel_init,
+};
+use arch::DefaultPagingMode;
+use eonix_mm::{
+    address::{Addr as _, AddrOps as _, PAddr, PRange, VAddr, VRange},
+    page_table::{PageAttribute, PagingMode as _, PTE},
+    paging::{NoAlloc, Page as GenericPage, PAGE_SIZE, PFN},
+};
+use eonix_runtime::context::ExecutionContext;
+use eonix_sync::LazyLock;
+
+static GLOBAL_PAGE_TABLE: LazyLock<
+    eonix_mm::page_table::PageTable<DefaultPagingMode, NoAlloc, KernelPageAccess>,
+> = LazyLock::new(|| unsafe {
+    GenericPage::with_raw(
+        DefaultPagingMode::KERNEL_ROOT_TABLE_PFN,
+        |root_table_page| eonix_mm::page_table::PageTable::with_root_table(root_table_page.clone()),
+    )
+});
+
+const HUGE_PAGE_LEN: usize = 1 << 21;
+
+const P_KERNEL_BSS_START: PAddr = PAddr::from_val(0x200000);
+const P_KIMAGE_START: PAddr = PAddr::from_val(0x400000);
+
+const V_KERNEL_PAGE_ARRAY_START: VAddr = VAddr::from(0xffffff8040000000);
+const V_KERNEL_BSS_START: VAddr = VAddr::from(0xffffffffc0200000);
+const KERNEL_BSS_LEN: usize = HUGE_PAGE_LEN;
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct E820MemMapEntry {
+    base: u64,
+    len: u64,
+    entry_type: u32,
+    acpi_attrs: u32,
+}
+
+#[repr(C)]
+#[derive(Copy, Clone)]
+struct BootLoaderData {
+    entry_count: u32,
+    entry_length: u32,
+
+    block_count_1k: u32,
+    block_count_64k: u32,
+
+    all_entries: [E820MemMapEntry; 42],
+}
+
+impl E820MemMapEntry {
+    const ENTRY_FREE: u32 = 1;
+    // const ENTRY_USED: u32 = 2;
+
+    fn is_free(&self) -> bool {
+        self.entry_type == Self::ENTRY_FREE
+    }
+
+    // fn is_used(&self) -> bool {
+    //     self.entry_type == Self::ENTRY_USED
+    // }
+
+    fn range(&self) -> PRange {
+        PRange::from(PAddr::from(self.base as usize)).grow(self.len as usize)
+    }
+}
+
+impl BootLoaderData {
+    // fn memory_size(&self) -> usize {
+    //     // The initial 1M is not counted in the E820 map. We add them to the total as well.
+    //     ((self.block_count_1k + 64 * self.block_count_64k) * 1024 + 1 * 1024 * 1024) as usize
+    // }
+
+    fn entries(&self) -> &[E820MemMapEntry] {
+        &self.all_entries[..self.entry_count as usize]
+    }
+
+    fn free_entries(&self) -> impl Iterator<Item = &E820MemMapEntry> {
+        self.entries().iter().filter(|entry| entry.is_free())
+    }
+}
+
+#[no_mangle]
+pub(self) extern "C" fn _kernel_init(bootloader_data: &mut BootLoaderData) -> ! {
+    // Map kernel BSS
+    for pte in GLOBAL_PAGE_TABLE.iter_kernel_levels(
+        VRange::from(V_KERNEL_BSS_START).grow(KERNEL_BSS_LEN),
+        &DefaultPagingMode::LEVELS[..3],
+    ) {
+        let attr = PageAttribute::PRESENT
+            | PageAttribute::WRITE
+            | PageAttribute::READ
+            | PageAttribute::HUGE
+            | PageAttribute::GLOBAL;
+
+        pte.set(PFN::from(P_KERNEL_BSS_START), attr.into());
+    }
+
+    unsafe {
+        // SAFETY: We've just mapped the area with sufficient length.
+        core::ptr::write_bytes(V_KERNEL_BSS_START.addr() as *mut (), 0, KERNEL_BSS_LEN);
+    }
+
+    let addr_max = bootloader_data
+        .free_entries()
+        .map(|entry| entry.range().end())
+        .max()
+        .expect("No free memory");
+
+    let pfn_max = PFN::from(addr_max.ceil());
+    let len_bytes_page_array = usize::from(pfn_max) * size_of::<RawPage>();
+
+    let count_huge_pages = len_bytes_page_array.div_ceil(HUGE_PAGE_LEN);
+
+    extern "C" {
+        // Definition inside linker script.
+        fn KIMAGE_PAGES();
+    }
+
+    let kimage_pages = unsafe { core::mem::transmute::<_, usize>(KIMAGE_PAGES as *const ()) };
+
+    let paddr_after_kimage = P_KIMAGE_START + kimage_pages * PAGE_SIZE;
+    let paddr_after_kimage_aligned = paddr_after_kimage.ceil_to(HUGE_PAGE_LEN);
+
+    let mut paddr_free = paddr_after_kimage_aligned;
+
+    // Map kernel page array.
+    for pte in GLOBAL_PAGE_TABLE.iter_kernel_levels(
+        VRange::from(V_KERNEL_PAGE_ARRAY_START).grow(HUGE_PAGE_LEN * count_huge_pages),
+        &DefaultPagingMode::LEVELS[..3],
+    ) {
+        let attr = PageAttribute::PRESENT
+            | PageAttribute::WRITE
+            | PageAttribute::READ
+            | PageAttribute::HUGE
+            | PageAttribute::GLOBAL;
+
+        pte.set(PFN::from(paddr_free), attr.into());
+
+        paddr_free = paddr_free + HUGE_PAGE_LEN;
+    }
+
+    unsafe {
+        // SAFETY: We've just mapped the area with sufficient length.
+        core::ptr::write_bytes(
+            V_KERNEL_PAGE_ARRAY_START.addr() as *mut (),
+            0,
+            count_huge_pages * HUGE_PAGE_LEN,
+        );
+    }
+
+    let paddr_unused_start = paddr_free;
+
+    for entry in bootloader_data.free_entries() {
+        let mut range = entry.range();
+
+        GlobalPageAlloc::mark_present(range);
+
+        if range.end() <= paddr_unused_start {
+            continue;
+        }
+
+        if range.start() < paddr_unused_start {
+            let (_, right) = range.split_at(paddr_unused_start);
+            range = right;
+        }
+
+        unsafe {
+            // SAFETY: We are in system initialization procedure where preemption is disabled.
+            GlobalPageAlloc::add_pages(range);
+        }
+    }
+
+    unsafe {
+        // SAFETY: We are in system initialization procedure where preemption is disabled.
+        GlobalPageAlloc::add_pages(PRange::new(PAddr::from(0x100000), PAddr::from(0x200000)));
+        GlobalPageAlloc::add_pages(PRange::new(paddr_after_kimage, paddr_after_kimage_aligned));
+    }
+
+    let (stack_bottom_addr, stack_pfn) = {
+        let kernel_stack_page = GenericPage::alloc_order_in(9, GlobalPageAlloc::early_alloc());
+        let stack_area = kernel_stack_page.as_memblk();
+
+        let stack_bottom_addr = stack_area
+            .addr()
+            .checked_add(stack_area.len())
+            .expect("The stack bottom should not be null");
+
+        let stack_pfn = kernel_stack_page.into_raw();
+
+        (stack_bottom_addr, stack_pfn)
+    };
+
+    let mut to_ctx = ExecutionContext::new();
+    to_ctx.set_interrupt(false);
+    to_ctx.set_sp(stack_bottom_addr.get());
+    to_ctx.call1(_init_on_new_stack, usize::from(stack_pfn));
+
+    to_ctx.switch_noreturn();
+}
+
+extern "C" fn _init_on_new_stack(early_kernel_stack_pfn: PFN) -> ! {
+    // Add the pages previously used by `_kernel_init` as a stack.
+    unsafe {
+        // SAFETY: We are in system initialization procedure where preemption is disabled.
+        GlobalPageAlloc::add_pages(PRange::new(PAddr::from(0x8000), PAddr::from(0x80000)));
+    }
+
+    init_localcpu();
+
+    extern "C" {
+        fn init_allocator();
+    }
+
+    unsafe { init_allocator() };
+
+    kernel::interrupt::init().unwrap();
+
+    kernel_init(early_kernel_stack_pfn)
+}

CMakeLists.txt
@@ -37,8 +37,7 @@ set(BOOTLOADER_SOURCES src/boot.s
                        src/mbr.S
                        )
 
-set(KERNEL_MAIN_SOURCES src/kinit.cpp
-                        src/kernel/async/lock.cc
+set(KERNEL_MAIN_SOURCES src/kernel/async/lock.cc
                         src/kernel/allocator.cc
                         src/kernel/mem/slab.cc
                         src/kernel/hw/acpi.cc
@@ -48,15 +47,12 @@ set(KERNEL_MAIN_SOURCES src/kinit.cpp
                         include/kernel/async/lock.hpp
                         include/kernel/mem/paging.hpp
                         include/kernel/mem/slab.hpp
-                        include/kernel/mem/types.hpp
-                        include/kernel/utsname.hpp
                         include/kernel/hw/acpi.hpp
                         include/kernel/hw/pci.hpp
                         include/kernel/hw/port.hpp
                         include/types/list.hpp
                         include/types/types.h
                         include/types/allocator.hpp
-                        include/kernel/log.hpp
                         )
 
 add_executable(kernel.out ${KERNEL_MAIN_SOURCES} ${BOOTLOADER_SOURCES})

arch/src/x86_64/context.rs
@@ -142,7 +142,8 @@ impl TaskContext {
             "mov %r15, %rcx",
             "mov %rbx, %r8",
             "mov %rbp, %rax",
-            "xor %rbp, %rbp",
+            "xor %rbp, %rbp", // NULL previous stack frame
+            "push %rbp",      // NULL return address.
             "jmp *%rax",
             options(att_syntax),
         );

arch/src/x86_64/init.rs
@@ -104,16 +104,13 @@ impl CPU {
 #[macro_export]
 macro_rules! define_smp_bootstrap {
     ($cpu_count:literal, $ap_entry:ident, $alloc_kstack:tt) => {
-        #[no_mangle]
         static BOOT_SEMAPHORE: core::sync::atomic::AtomicU64 =
             core::sync::atomic::AtomicU64::new(0);
-        #[no_mangle]
         static BOOT_STACK: core::sync::atomic::AtomicU64 =
             core::sync::atomic::AtomicU64::new(0);
 
-        #[no_mangle]
         static CPU_COUNT: core::sync::atomic::AtomicU64 =
-            core::sync::atomic::AtomicU64::new(0);
+            core::sync::atomic::AtomicU64::new(1);
 
         core::arch::global_asm!(
             r#"
@@ -122,11 +119,11 @@ macro_rules! define_smp_bootstrap {
         .globl ap_bootstrap
         .type ap_bootstrap, @function
         ap_bootstrap:
-            ljmp $0x0, $.Lap1
+            ljmp $0x0, $2f
 
-        .Lap1:
+        2:
             # we use the shared gdt for cpu bootstrapping
-            lgdt .Lshared_gdt_desc
+            lgdt EARLY_GDT_DESCRIPTOR
 
             # set msr
             mov $0xc0000080, %ecx
@@ -148,14 +145,10 @@ macro_rules! define_smp_bootstrap {
             or $0x80010001, %eax
             mov %eax, %cr0
 
-            ljmp $0x08, $.Lap_bootstrap_end
-
-        .align 16
-        .Lshared_gdt_desc:
-            .8byte 0x0000000000005f
+            ljmp $0x08, $2f
 
         .code64
-        .Lap_bootstrap_end:
+        2:
             mov $0x10, %ax
             mov %ax, %ds
             mov %ax, %es
@@ -164,21 +157,21 @@ macro_rules! define_smp_bootstrap {
             xor %rsp, %rsp
             xor %rax, %rax
             inc %rax
-        1:
+        2:
             xchg %rax, {BOOT_SEMAPHORE}
             cmp $0, %rax
-            je 1f
+            je 2f
             pause
-            jmp 1b
+            jmp 2b
 
-        1:
+        2:
             mov {BOOT_STACK}, %rsp # Acquire
             cmp $0, %rsp
-            jne 1f
+            jne 2f
             pause
-            jmp 1b
+            jmp 2b
 
-        1:
+        2:
             xor %rax, %rax
             mov %rax, {BOOT_STACK} # Release
             xchg %rax, {BOOT_SEMAPHORE}
@@ -190,7 +183,7 @@ macro_rules! define_smp_bootstrap {
             jmp {AP_ENTRY}
             .popsection
             "#,
-            KERNEL_PML4 = const 0x2000,
+            KERNEL_PML4 = const 0x1000,
             BOOT_SEMAPHORE = sym BOOT_SEMAPHORE,
             BOOT_STACK = sym BOOT_STACK,
             CPU_COUNT = sym CPU_COUNT,
@@ -200,7 +193,7 @@ macro_rules! define_smp_bootstrap {
 
         pub unsafe fn wait_cpus_online() {
             use core::sync::atomic::Ordering;
-            while CPU_COUNT.load(Ordering::Acquire) != $cpu_count - 1 {
+            while CPU_COUNT.load(Ordering::Acquire) != $cpu_count {
                 if BOOT_STACK.load(Ordering::Acquire) == 0 {
                     let stack_bottom = $alloc_kstack as u64;
                     BOOT_STACK.store(stack_bottom, Ordering::Release);

arch/src/x86_64/interrupt.rs
@@ -482,7 +482,7 @@ impl InterruptControl {
             pause();
         }
 
-        icr.write(0xc4601);
+        icr.write(0xc4607);
         while icr.read() & 0x1000 != 0 {
             pause();
         }

arch/src/x86_64/mm.rs
@@ -9,7 +9,7 @@ use eonix_mm::{
 
 pub const PAGE_SIZE: usize = 0x1000;
 
-const KERNEL_PML4_PFN: PFN = PFN::from_val(0x2000 >> 12);
+const KERNEL_PML4_PFN: PFN = PFN::from_val(0x1000 >> 12);
 
 const PA_P: u64 = 0x001;
 const PA_RW: u64 = 0x002;
@@ -156,26 +156,16 @@ impl RawAttribute for PageAttribute64 {
             page_attr |= PageAttribute::ANONYMOUS;
         }
 
-        Some(page_attr)
-    }
-
-    fn from_table_attr(table_attr: TableAttribute) -> Self {
-        let mut raw_attr = PA_RW;
-
-        for attr in table_attr.iter() {
-            match attr {
-                TableAttribute::PRESENT => raw_attr |= PA_P,
-                TableAttribute::GLOBAL => raw_attr |= PA_G,
-                TableAttribute::USER => raw_attr |= PA_US,
-                TableAttribute::ACCESSED => raw_attr |= PA_A,
-                _ => unreachable!("Invalid table attribute"),
-            }
+        if self.0 & PA_PS != 0 {
+            page_attr |= PageAttribute::HUGE;
         }
 
-        Self(raw_attr)
+        Some(page_attr)
     }
+}
 
-    fn from_page_attr(page_attr: PageAttribute) -> Self {
+impl From<PageAttribute> for PageAttribute64 {
+    fn from(page_attr: PageAttribute) -> Self {
         let mut raw_attr = PA_NXE;
 
         for attr in page_attr.iter() {
@@ -191,7 +181,26 @@ impl RawAttribute for PageAttribute64 {
                 PageAttribute::COPY_ON_WRITE => raw_attr |= PA_COW,
                 PageAttribute::MAPPED => raw_attr |= PA_MMAP,
                 PageAttribute::ANONYMOUS => raw_attr |= PA_ANON,
+                PageAttribute::HUGE => raw_attr |= PA_PS,
                 _ => unreachable!("Invalid page attribute"),
+            }
+        }
+
+        Self(raw_attr)
+    }
+}
+
+impl From<TableAttribute> for PageAttribute64 {
+    fn from(table_attr: TableAttribute) -> Self {
+        let mut raw_attr = PA_RW;
+
+        for attr in table_attr.iter() {
+            match attr {
+                TableAttribute::PRESENT => raw_attr |= PA_P,
+                TableAttribute::GLOBAL => raw_attr |= PA_G,
+                TableAttribute::USER => raw_attr |= PA_US,
+                TableAttribute::ACCESSED => raw_attr |= PA_A,
+                _ => unreachable!("Invalid table attribute"),
             }
         }
 

crates/eonix_mm/src/page_table/page_table.rs
@@ -42,6 +42,13 @@ where
     A: PageAlloc,
     X: PageAccess,
 {
+    pub fn with_root_table(root_table_page: Page<A>) -> Self {
+        Self {
+            root_table_page,
+            phantom: PhantomData,
+        }
+    }
+
     pub fn new_in<A1: PageAlloc>(kernel_root_table_page: &Page<A1>, alloc: A) -> Self {
         let new_root_table_page = Page::alloc_in(alloc);
         let new_table_data = X::get_ptr_for_page(&new_root_table_page);
@@ -65,10 +72,7 @@ where
             root_page_table.index_mut(idx).take();
         }
 
-        Self {
-            root_table_page: new_root_table_page,
-            phantom: PhantomData,
-        }
+        Self::with_root_table(new_root_table_page)
     }
 
     pub fn addr(&self) -> PAddr {
@@ -87,7 +91,7 @@ where
     }
 
     pub fn iter_kernel(&self, range: VRange) -> impl Iterator<Item = &mut M::Entry> {
-        Self::iter_kernel_levels(self, range, M::LEVELS)
+        self.iter_kernel_levels(range, M::LEVELS)
     }
 
     /// Iterates over the kernel space entries in the page table for the specified levels.

crates/eonix_mm/src/page_table/pte.rs
@@ -23,10 +23,11 @@ bitflags! {
         const COPY_ON_WRITE = 256;
         const MAPPED = 512;
         const ANONYMOUS = 1024;
+        const HUGE = 2048;
     }
 }
 
-pub trait RawAttribute: Copy {
+pub trait RawAttribute: Copy + From<PageAttribute> + From<TableAttribute> {
     /// Create a new attribute representing a non-present page.
     fn null() -> Self;
 
@@ -43,18 +44,6 @@ pub trait RawAttribute: Copy {
     /// # Panic
     /// The implementor should panic if invalid combinations of flags are present.
     fn as_page_attr(self) -> Option<PageAttribute>;
-
-    /// Convert the attribute to a raw value.
-    ///
-    /// # Panic
-    /// The implementor should panic if invalid combinations of flags are present.
-    fn from_table_attr(table_attr: TableAttribute) -> Self;
-
-    /// Convert the attribute to a raw value.
-    ///
-    /// # Panic
-    /// The implementor should panic if invalid combinations of flags are present.
-    fn from_page_attr(page_attr: PageAttribute) -> Self;
 }
 
 pub trait PTE: Sized {

commit 20b12dfa221a87471f2fe49a3f5e7730bce3c2dc

sysinit: pointee type should be u8 when using ptr::write_bytes
Using *const (), no bytes are written to the position, which might
result in uninitialized memory access.

Fixes: ebd3d1224c01 ("change(x86): optimize bootstrap code, remove kinit.cpp")
Fixes: 191877a3acd0 ("feat(hal): impl basic single hart bootstrap for riscv64")
Signed-off-by: greatbridf <greatbridf@icloud.com>

crates/eonix_hal/src/arch/riscv64/bootstrap.rs
@@ -186,7 +186,7 @@ fn setup_kernel_page_table(alloc: BasicPageAllocRef) {
 
     unsafe {
         core::ptr::write_bytes(
-            KERNEL_BSS_START.addr() as *mut (),
+            KERNEL_BSS_START.addr() as *mut u8,
             0,
             bss_length,
         );

src/kernel_init.rs
@@ -41,7 +41,7 @@ fn setup_kernel_page_array(alloc: BasicPageAllocRef, count_pages: usize) {
     unsafe {
         // SAFETY: We've just mapped the area with sufficient length.
         core::ptr::write_bytes(
-            V_KERNEL_PAGE_ARRAY_START.addr() as *mut (),
+            V_KERNEL_PAGE_ARRAY_START.addr() as *mut u8,
             0,
             count_pages * PAGE_SIZE,
         );
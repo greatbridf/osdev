5732b46e9a58e2a9954457e9b7784061a79097e4
greatbridf <greatbridf@icloud.com>
2026-03-05 23:20:26 +0800

    util: remove dont_check macro
    This was used in `dump_mount()` and is now unused after rewrite of
    `dump_mount()` in commit 181c1896667d ("vfs: move /proc/mounts
    population to mount.rs")"). Remove it now.
    
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,23 +1,14 @@
 #[allow(dead_code)]
 pub type KResult<T> = Result<T, u32>;
 
-macro_rules! dont_check {
-    ($arg:expr) => {
-        match $arg {
-            Ok(_) => (),
-            Err(_) => (),
-        }
-    };
-}
+pub(crate) use alloc::boxed::Box;
+pub(crate) use alloc::string::String;
+pub(crate) use alloc::vec;
+pub(crate) use alloc::vec::Vec;
+pub(crate) use core::fmt::Write;
+pub(crate) use core::marker::PhantomData;
+pub(crate) use core::str;
 
-pub(crate) use dont_check;
-
-pub(crate) use crate::kernel::console::{
-    print, println, println_debug, println_fatal, println_info, println_trace, println_warn,
-};
-
-pub(crate) use alloc::{boxed::Box, string::String, vec, vec::Vec};
-
-pub(crate) use core::{fmt::Write, marker::PhantomData, str};
+pub(crate) use eonix_log::{println_debug, println_warn};
 
 pub use crate::sync::Spin;

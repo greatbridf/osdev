greatbridf/osdev

commit 576c43db2d2be202b6df3777fbbe16e73c7f7722

fix(CoW): check both PRESENT and MAPPED bits when doing cow

src/kernel/mem/mm_list.rs
@@ -753,7 +753,7 @@ where
             .as_page_attr()
             .expect("Not a page attribute");
 
-        if !from_attr.contains(PageAttribute::PRESENT) {
+        if !from_attr.intersects(PageAttribute::PRESENT | PageAttribute::MAPPED) {
             return;
         }
 

commit 05a498a95c5e2075c7fad802939e02855ce353e8

mm: copy mmapped PTEs as a whole when doing CoWs
The previous implementation in commit 576c43db2d2b ("fix(CoW): check
both PRESENT and MAPPED bits when doing cow") decided to treat mmapped
PTEs and normal anonymous PTEs the same, which leads to unnecessary gets
to empty pages (since we don't treat mapped PTEs as CoW now so the extra
reference will never be used or put in handle_mmap()) during the process
we make the PTE CoW.

This issue combines with yet another two bugs that:
1. We get a reference to the empty page when we setup mapped PTEs, so
   there will always be "enough" refcounts to the empty page.
2. We forget to check whether the PTE is present when we drop the
   MMList.

Our current implementation happens to be working "just fine" and
produces no errors and crashes. But the whole system is ill-formed and
needs repairs.

The solution is to add checks to skip non-present PTEs when we drop the
MMList, and to copy the entire PTE to the new PTE when we are setting up
CoW, if the PTE is a mapped one. So now, when the PTE needs special
treatments in the fault handler, the entry is either a CoW one with its
PFN valid, or a mapped one with its PFN unset.

Fixes: 576c43db2d2b ("fix(CoW): check both PRESENT and MAPPED bits when doing cow")
Signed-off-by: greatbridf <greatbridf@icloud.com>

src/kernel/mem/mm_list.rs
@@ -245,19 +245,23 @@ impl Drop for MMListInner {
             let range = area.range.as_ref(&self.lock).clone();
 
             if area.is_shared() {
-                for pte in self.page_table.iter_user(range) {
-                    // XXX: Fix me
-                    let _ = pte.take();
-                    // let raw_page = RawPagePtr::from(pfn);
-                    // if raw_page.refcount().fetch_sub(1, Ordering::Relaxed) == 1 {
-                    //     unsafe { Page::from_raw(pfn) };
-                    // }
+                unimplemented!("Shared mapping is not yet implemented");
+            }
+
+            for pte in self.page_table.iter_user(range) {
+                let (pfn, raw_attr) = pte.take();
+                let attr = raw_attr.as_page_attr().expect("Not a page");
+
+                if !attr.contains(PageAttribute::PRESENT) {
+                    continue;
                 }
-            } else {
-                for pte in self.page_table.iter_user(range) {
-                    let (pfn, _) = pte.take();
-                    unsafe { Folio::from_raw(pfn) };
-                }
+
+                unsafe {
+                    // SAFETY: Present PTEs always corresponding to a valid
+                    //         Folio that was previously installed through
+                    //         `Folio::into_raw()`.
+                    Folio::from_raw(pfn)
+                };
             }
         }
 
@@ -699,9 +703,17 @@ where
             .as_page_attr()
             .expect("Not a page attribute");
 
-        if !from_attr.intersects(PageAttribute::PRESENT | PageAttribute::MAPPED)
-        {
+        if from_attr.intersects(PageAttribute::MAPPED) {
+            // Copy non-installed mapped PTEs directly to the new PTE and delay
+            // its handling till the page fault.
+            let (pfn, attr) = from.get();
+            self.set(pfn, attr);
             return;
+        }
+
+        if !from_attr.intersects(PageAttribute::PRESENT) {
+            // Shouldn't really see this happen...
+            unreachable!("Non present PTE");
         }
 
         from_attr.remove(PageAttribute::WRITE | PageAttribute::DIRTY);
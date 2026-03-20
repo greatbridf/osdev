greatbridf/osdev

commit 9b18fc867f2ec78e2d2ef1705154d92e8a5c9ad4

WIP: mem: remove unused MemArea member functions
Remove
- `MemArea::split()`
- `AreaList::take()`
- `RangeProtected::get_mut()`

Signed-off-by: greatbridf <greatbridf@icloud.com>

src/kernel/mem/mm_list/mm_area.rs
@@ -1,10 +1,9 @@
 use alloc::sync::Arc;
 use core::cell::UnsafeCell;
-use core::cmp;
 use core::mem::ManuallyDrop;
 use core::sync::atomic::{AtomicU32, Ordering};
 
-use eonix_mm::address::{AddrOps as _, VAddr, VRange};
+use eonix_mm::address::{VAddr, VRange};
 use eonix_mm::page_table::{PageAttribute, RawAttribute, PTE};
 use eonix_mm::paging::{Folio as _, PFN};
 use eonix_sync::Mutex;
@@ -187,10 +186,6 @@ impl RangeProtected {
             // return a mutable reference.
             &mut *self.0.get()
         }
-    }
-
-    pub fn get_mut(&mut self) -> &mut VRange {
-        self.0.get_mut()
     }
 }
 
@@ -377,11 +372,6 @@ impl AreaList {
             Some(area)
         })
     }
-
-    // TODO: For backwards compatibility. Remove this.
-    pub fn take(&mut self) -> impl IntoIterator<Item = Arc<MemArea>> {
-        self.areas.take()
-    }
 }
 
 impl MemArea {
@@ -452,40 +442,6 @@ impl MemArea {
         };
         modify(arc_mut);
         arc
-    }
-
-    pub fn split(mut self, at: VAddr) -> (Option<Self>, Option<Self>) {
-        assert!(at.is_page_aligned());
-        let range = self.range.get_mut();
-
-        match (*range).cmp(&VRange::from(at)) {
-            cmp::Ordering::Less => (Some(self), None),
-            cmp::Ordering::Greater => (None, Some(self)),
-            cmp::Ordering::Equal => {
-                let diff = at - range.start();
-                if diff == 0 {
-                    return (None, Some(self));
-                }
-
-                let right = Self {
-                    range: RangeProtected::new(VRange::new(at, range.end())),
-                    flags: self.flags.clone(),
-                    lock: Mutex::new(AreaLock::_new()),
-                    link: Link::rbtree(),
-                    mapping: match &self.mapping {
-                        Mapping::Anonymous => Mapping::Anonymous,
-                        Mapping::File(mapping) => {
-                            Mapping::File(mapping.offset(diff))
-                        }
-                    },
-                };
-
-                let new_range = range.shrink(range.end() - at);
-
-                *self.range.get_mut() = new_range;
-                (Some(self), Some(right))
-            }
-        }
     }
 
     pub fn handle_cow(&self, pfn: &mut PFN, attr: &mut PageAttribute) {

commit 74d6122acffef7539ecf127d5ed714d740fc3daa

mm: update mappings' offsets when splitting areas
I notice that when we isolate the areas for protecting or unmapping, we
forgot to update the underlying mappings' offsets. This may cause wrong
offset passed to read() in fault handlers, despite we haven't observed
the problem yet.

Fix this by refactoring the isolate() logic: we recreate every area
within the isolate range, call newly defined split functions and handle
the offset correctly.

Fixes: 9b18fc867f2e ("WIP: mem: remove unused MemArea member functions")
Fixes: f37b288240ba ("WIP: mem: reimpl `MMList::protect` using `isolate()`")
Signed-off-by: greatbridf <greatbridf@icloud.com>

src/kernel/mem/mm_list/mapping.rs
@@ -32,6 +32,10 @@ impl AnonMapping {
     pub const fn new() -> Mapping {
         Mapping::Anonymous(Self::_new())
     }
+
+    fn split(&self, _offset: usize) -> (Self, Self) {
+        (Self::_new(), Self::_new())
+    }
 }
 
 impl FileMapping {
@@ -46,15 +50,35 @@ impl FileMapping {
         }
     }
 
-    pub fn offset(&self, offset: usize) -> Self {
-        if self.length <= offset {
-            Self::new(self.page_cache.clone(), self.offset + self.length, 0)
+    fn split(&self, offset: usize) -> (Self, Self) {
+        let (left_len, right_len);
+
+        if offset >= self.length {
+            left_len = self.length;
+            right_len = 0;
         } else {
-            Self::new(
-                self.page_cache.clone(),
-                self.offset + offset,
-                self.length - offset,
-            )
+            left_len = offset;
+            right_len = self.length - offset;
+        }
+
+        (
+            Self::new(self.page_cache.clone(), self.offset, left_len),
+            Self::new(self.page_cache.clone(), self.offset + offset, right_len),
+        )
+    }
+}
+
+impl Mapping {
+    pub(super) fn split(&self, offset: usize) -> (Self, Self) {
+        match self {
+            Mapping::Anonymous(anon_mapping) => {
+                let (l, r) = anon_mapping.split(offset);
+                (Self::Anonymous(l), Self::Anonymous(r))
+            }
+            Mapping::File(file_mapping) => {
+                let (l, r) = file_mapping.split(offset);
+                (Self::File(l), Self::File(r))
+            }
         }
     }
 }

src/kernel/mem/mm_list/mm_area.rs
@@ -267,9 +267,9 @@ impl AreaList {
         &mut self, isolate_range: &VRange, lock: &mut MemListLock,
     ) -> impl Iterator<Item = Arc<MemArea>> {
         let mut ret_areas = LinkedList::new(ListAdapter::NEW);
-        let begin = VRange::from(isolate_range.start());
-
-        let mut cursor = self.areas.upper_bound_mut(Bound::Included(&begin));
+        let mut cursor = self.areas.upper_bound_mut(Bound::Included(
+            &VRange::from(isolate_range.start()),
+        ));
 
         while !cursor.is_null() {
             let range = {
@@ -288,48 +288,44 @@ impl AreaList {
                 range.clone()
             };
 
-            let area = cursor.as_cursor().clone_pointer().unwrap();
-            let list_lock = &mut self.lock;
-            let mut area_lock = area.lock.lock().await;
-            let area_lock = &mut area_lock;
+            let mut area = cursor.remove().unwrap();
 
-            let (l, m, r) = range.mask_with_checked(isolate_range).unwrap();
+            {
+                // Exclude all concurrent faulters.
+                area.lock.lock().await;
 
-            match (l, r) {
-                (None, None) => {
-                    // Fully covered, just remove it.
-                    let area = cursor.remove().unwrap();
-                    unsafe {
-                        area.link.to_list();
-                    }
-                    ret_areas.push_back(area);
-                    continue;
-                }
-                (None, Some(rem)) | (Some(rem), None) => {
-                    // Overflow on one side, change the old area's range and
-                    // return the newly created area.
-                    let range = area.range.as_mut(lock, list_lock, area_lock);
-                    *range = rem;
-                }
-                (Some(left), Some(right)) => {
-                    // Overflow on both sides, change the old area's range to
-                    // the left part, create a new area for the right part and
-                    // return the middle part.
-                    let range = area.range.as_mut(lock, list_lock, area_lock);
-                    *range = left;
-
-                    cursor.insert_after(area.clone_and_modify(lock, |area| {
-                        area.range = RangeProtected::new(right);
-                    }));
-                }
+                // XXX: This is slightly racy and the assertion may not hold if
+                //      one hasn't dropped the arc after releasing the lock.
+                //      Let's observe if we can actually trigger this...
+                assert_eq!(Arc::strong_count(&area), 1, "Shared isolated area");
             }
 
-            let ret_range = area.clone_and_modify(lock, |area| {
-                area.range = RangeProtected::new(m);
-                area.link = Link::list();
-            });
+            let left_overflow = range.start() < isolate_range.start();
+            let right_overflow = range.end() > isolate_range.end();
 
-            ret_areas.push_back(ret_range);
+            if left_overflow {
+                let left;
+                let offset = isolate_range.start() - range.start();
+
+                (left, area) = area.split(offset, lock);
+
+                cursor.insert_before(left);
+            }
+
+            if right_overflow {
+                let right;
+                let offset = isolate_range.end() - range.start();
+
+                (area, right) = area.split(offset, lock);
+
+                cursor.insert_before(right);
+            }
+
+            unsafe {
+                // SAFETY: The area is unlinked and exclusive to us.
+                area.link.to_list();
+            }
+            ret_areas.push_back(area);
         }
 
         // We are returning the area, and the area should be by default
@@ -423,16 +419,37 @@ impl MemArea {
         }
     }
 
-    fn clone_and_modify(
-        &self, lock: &MemListLock, modify: impl FnOnce(&mut MemArea),
-    ) -> Arc<Self> {
-        let mut arc = Arc::new(self.clone(lock));
-        let arc_mut = unsafe {
-            // SAFETY: We are the only owner.
-            Arc::get_mut(&mut arc).unwrap_unchecked()
+    fn split(
+        &self, offset: usize, lock: &MemListLock,
+    ) -> (Arc<Self>, Arc<Self>) {
+        let (begin, mid, end) = {
+            let range = self.range.as_ref(lock);
+            let begin = range.start();
+            let mid = begin + offset;
+            let end = range.end();
+
+            (begin, mid, end)
         };
-        modify(arc_mut);
-        arc
+
+        let (left_mapping, right_mapping) = self.mapping.split(offset);
+
+        let left = Arc::new(Self {
+            range: RangeProtected::new(VRange::new(begin, mid)),
+            flags: self.flags.clone(),
+            lock: Mutex::new(AreaLock::_new()),
+            link: Link::rbtree(),
+            mapping: left_mapping,
+        });
+
+        let right = Arc::new(Self {
+            range: RangeProtected::new(VRange::new(mid, end)),
+            flags: self.flags.clone(),
+            lock: Mutex::new(AreaLock::_new()),
+            link: Link::rbtree(),
+            mapping: right_mapping,
+        });
+
+        (left, right)
     }
 
     pub fn handle_cow(&self, pfn: &mut PFN, attr: &mut PageAttribute) {